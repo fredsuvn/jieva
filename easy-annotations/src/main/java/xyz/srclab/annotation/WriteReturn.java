37687ee6aa29020d49780a354931c06c1d1cdbd0
SrcLab <fredsuvn@163.com>
2020-03-31 00:07:33 +0800

    add WriteReturn

---
+++ easy-annotations/src/main/java/xyz/srclab/annotation/WriteReturn.java
@@ -0,0 +1,14 @@
+package xyz.srclab.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Represents the parameter would be write and return.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+        ElementType.PARAMETER,
+})
+public @interface WriteReturn {
+}
