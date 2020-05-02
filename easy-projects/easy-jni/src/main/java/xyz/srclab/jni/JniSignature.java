5fd45652a5280d3caad929f3e3b75248dcbb70e8
SrcLab <fredsuvn@163.com>
2020-05-02 16:36:33 +0800

    remove jni

--- easy-projects/easy-jni/src/main/java/xyz/srclab/jni/JniSignature.java
+++ easy-projects/easy-jni/src/main/java/xyz/srclab/jni/JniSignature.java
@@ -1,27 +1,23 @@
 package xyz.srclab.jni;
 
-import xyz.srclab.common.reflect.signature.SignatureHelper;
-
-import java.lang.reflect.Method;
-
 public class JniSignature {
 
-    public static String sign(Class<?> cls) {
-        return SignatureHelper.signClass(cls);
-    }
-
-    public static String signMethod(Method method) {
-        return signMethod(method.getParameterTypes(), method.getReturnType());
-    }
-
-    public static String signMethod(Class<?>[] parameterTypes, Class<?> returnType) {
-        StringBuilder buf = new StringBuilder();
-        buf.append("(");
-        for (Class<?> parameterType : parameterTypes) {
-            buf.append(sign(parameterType));
-        }
-        buf.append(")");
-        buf.append(returnType);
-        return buf.toString();
-    }
+    //public static String sign(Class<?> cls) {
+    //    return SignatureHelper.signClass(cls);
+    //}
+    //
+    //public static String signMethod(Method method) {
+    //    return signMethod(method.getParameterTypes(), method.getReturnType());
+    //}
+    //
+    //public static String signMethod(Class<?>[] parameterTypes, Class<?> returnType) {
+    //    StringBuilder buf = new StringBuilder();
+    //    buf.append("(");
+    //    for (Class<?> parameterType : parameterTypes) {
+    //        buf.append(sign(parameterType));
+    //    }
+    //    buf.append(")");
+    //    buf.append(returnType);
+    //    return buf.toString();
+    //}
 }
