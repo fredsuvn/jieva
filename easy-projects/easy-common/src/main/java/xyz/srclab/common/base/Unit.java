697e0602c07edf046563993e8ea79f935889b946
sunqian <fredsuvn@163.com>
2020-06-22 15:03:07 +0800

    optimize unit

--- easy-projects/easy-common/src/main/java/xyz/srclab/common/base/Unit.java
+++ easy-projects/easy-common/src/main/java/xyz/srclab/common/base/Unit.java
@@ -1,5 +1,13 @@
 package xyz.srclab.common.base;
 
+import xyz.srclab.common.record.Record;
+
+import java.lang.annotation.Annotation;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAmount;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Predicate;
 
 /**
@@ -18,11 +26,28 @@ public class Unit {
     private static final class UnitPredicateHolder {
 
         private static final Class<?>[] PASS_TYPES = {
-
+                Enum.class,
+                Annotation.class,
+                CharSequence.class,
+                Number.class,
+                Date.class,
+                TemporalAccessor.class,
+                TemporalAmount.class,
         };
 
         private static final Class<?>[] FAIL_TYPES = {
-
+                Record.class,
+                Collection.class,
+                Map.class,
+                Object[].class,
+                boolean[].class,
+                byte[].class,
+                short[].class,
+                char[].class,
+                int[].class,
+                long[].class,
+                float[].class,
+                double[].class,
         };
 
         public static final Predicate<Class<?>> INSTANCE = newUnitPredicateBuilder()
