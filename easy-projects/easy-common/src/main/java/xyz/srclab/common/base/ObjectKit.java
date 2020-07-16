c0b0ee6aa28227e5c665dd98b9c061d58b741467
SrcLab <fredsuvn@163.com>
2020-07-17 00:17:22 +0800

    add ObjectKit

---
+++ easy-projects/easy-common/src/main/java/xyz/srclab/common/base/ObjectKit.java
@@ -0,0 +1,18 @@
+package xyz.srclab.common.base;
+
+import xyz.srclab.annotation.Nullable;
+
+public class ObjectKit {
+
+    public static int hashCode(@Nullable Object any) {
+        return 0;
+    }
+
+    public static boolean equals(@Nullable Object a, @Nullable Object b) {
+        return false;
+    }
+
+    public static String toString(@Nullable Object any) {
+        return "";
+    }
+}
