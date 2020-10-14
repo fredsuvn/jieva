c0eb0b46646e94f6628fa47a43cf49e44fe0a1da
SrcLab <fredsuvn@163.com>
2020-10-15 01:15:24 +0800

    fix

--- srclab-common/src/main/kotlin/xyz/srclab/common/base/About.kt
+++ srclab-common/src/main/kotlin/xyz/srclab/common/base/About.kt
@@ -52,6 +52,103 @@ enum class About(
     }
 }
 
+interface Version : Comparable<Version> {
+
+    @Suppress("INAPPLICABLE_JVM_NAME")
+    val major: Int
+        @JvmName("major") get
+
+    @Suppress("INAPPLICABLE_JVM_NAME")
+    val minor: Int
+        @JvmName("minor") get
+
+    @Suppress("INAPPLICABLE_JVM_NAME")
+    val patch: Int
+        @JvmName("patch") get
+
+    @Suppress("INAPPLICABLE_JVM_NAME")
+    val preRelease: List<String>
+        @JvmName("preRelease") get
+
+    @Suppress("INAPPLICABLE_JVM_NAME")
+    val build: List<String>
+        @JvmName("build") get
+
+    class Builder {
+
+        private var major = 0
+        private var minor = 0
+        private var patch = 0
+        private var preRelease: List<String>? = null
+        private var build: List<String>? = null
+
+        fun major(value: Int) = apply {
+            major = value
+        }
+
+        fun minor(value: Int) = apply {
+            minor = value
+        }
+
+        fun patch(value: Int) = apply {
+            patch = value
+        }
+
+
+        fun build(): Version {
+            return object : Version {
+
+
+
+            }
+        }
+
+        private class VersionImpl(
+            override val major: Int,
+            override val minor: Int,
+            override val patch: Int,
+            override val preRelease: List<String>,
+            override val build: List<String>
+        ) : Version {
+
+            override fun compareTo(other: Version): Int {
+                if (major > other.major) {
+                    return 1
+                }
+                if (major < other.major) {
+                    return -1
+                }
+                if (minor > other.minor) {
+                    return 1
+                }
+                if (minor < other.minor) {
+                    return -1
+                }
+                if (patch > other.patch) {
+                    return 1
+                }
+                if (patch < other.patch) {
+                    return -1
+                }
+                if (preRelease.isEmpty() && other.preRelease.isEmpty()) {
+                    return 0
+                }
+                if (preRelease.isNotEmpty() && other.preRelease.isEmpty()) {
+                    return 0
+                }
+                if (preRelease.isEmpty() && other.preRelease.isNotEmpty()) {
+                    return 0
+                }
+            }
+        }
+    }
+
+    companion object {
+
+
+    }
+}
+
 fun aboutBoat(): About {
     return About.current
 }
