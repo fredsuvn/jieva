c125cd62fde8add0d583b2410ac8d60e3339599a
SrcLab <fredsuvn@163.com>
2020-10-19 00:19:54 +0800

    add Let

---
+++ srclab-common/src/main/kotlin/xyz/srclab/common/base/Let.kt
@@ -0,0 +1,42 @@
+package xyz.srclab.common.base
+
+/**
+ * This interface is used for java:
+ * ```
+ * Date date = Let.forAny(intValue)
+ *     .then(StringUtils::intToString)
+ *     .then(DateUtils::stringToDate)
+ *     .get()
+ * ```
+ * Same as:
+ * ```
+ * String stringValue = StringUtils.intToString(intValue)
+ * Date dateValue = DateUtils.stringToDate(stringValue)
+ * ```
+ */
+interface Let<T> {
+
+    fun <R> then(action: (T) -> R): Let<R>
+
+    fun get(): T
+
+    companion object {
+
+        @JvmStatic
+        fun <T> forAny(any: T): Let<T> {
+            return LetImpl(any)
+        }
+    }
+}
+
+private class LetImpl<T>(private var any: T) : Let<T> {
+
+    override fun <R> then(action: (T) -> R): Let<R> {
+        any = action(any).asAny()
+        return this.asAny()
+    }
+
+    override fun get(): T {
+        return any
+    }
+}
