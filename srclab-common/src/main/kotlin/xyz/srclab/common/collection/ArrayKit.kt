86df58fc0956fd5de5d750500f348decf5f1dfbc
sunqian <fredsuvn@163.com>
2020-10-23 16:08:08 +0800

    fix ArrayKit.kt

--- srclab-common/src/main/kotlin/xyz/srclab/common/collection/ArrayKit.kt
+++ srclab-common/src/main/kotlin/xyz/srclab/common/collection/ArrayKit.kt
@@ -4,23 +4,24 @@
 package xyz.srclab.common.collection
 
 import xyz.srclab.common.base.asAny
-import xyz.srclab.common.reflect.rawClass
 import java.lang.reflect.GenericArrayType
 import java.lang.reflect.Type
 
 val Type.isArray: Boolean
     @JvmName("isArray") get() {
         return when (this) {
+            is Class<*> -> this.isArray
             is GenericArrayType -> true
-            else -> this.rawClass.isArray
+            else -> false
         }
     }
 
-val Type.componentType: Type
+val Type.componentType: Type?
     get() {
         return when (this) {
+            is Class<*> -> this.componentType
             is GenericArrayType -> this.genericComponentType
-            else -> this.rawClass.componentType
+            else -> null
         }
     }
 
@@ -29,128 +30,142 @@ fun <A> Class<*>.componentTypeToArrayInstance(length: Int): A {
 }
 
 fun <A> Class<*>.arrayTypeToArrayInstance(length: Int): A {
-    return java.lang.reflect.Array.newInstance(this.componentType, length).asAny()
+    if (!this.isArray) {
+        throw IllegalArgumentException("$this is not an array type.")
+    }
+    return this.componentType.componentTypeToArrayInstance(length)
 }
 
 fun <T> Array<T>.asList(): MutableList<T> {
-    return java.util.Arrays.asList(*this)
+    val bridge = object : ArrayBridge<T> {
+        override val size: Int = this@asList.size
+
+        override fun get(index: Int): T {
+            return this@asList[index]
+        }
+
+        override fun set(index: Int, element: T) {
+            this@asList[index] = element
+        }
+    }
+    return ArrayBridgeList(bridge)
 }
 
 fun BooleanArray.asList(): MutableList<Boolean> {
-    val array = this
     val bridge = object : ArrayBridge<Boolean> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Boolean {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Boolean) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
 }
 
 fun ByteArray.asList(): MutableList<Byte> {
-    val array = this
     val bridge = object : ArrayBridge<Byte> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Byte {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Byte) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
 }
 
 fun ShortArray.asList(): MutableList<Short> {
-    val array = this
     val bridge = object : ArrayBridge<Short> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Short {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Short) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
 }
 
 fun CharArray.asList(): MutableList<Char> {
-    val array = this
     val bridge = object : ArrayBridge<Char> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Char {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Char) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
 }
 
 fun IntArray.asList(): MutableList<Int> {
-    val array = this
     val bridge = object : ArrayBridge<Int> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Int {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Int) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
 }
 
 fun LongArray.asList(): MutableList<Long> {
-    val array = this
     val bridge = object : ArrayBridge<Long> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Long {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Long) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
 }
 
 fun FloatArray.asList(): MutableList<Float> {
-    val array = this
     val bridge = object : ArrayBridge<Float> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Float {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Float) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
 }
 
 fun DoubleArray.asList(): MutableList<Double> {
-    val array = this
     val bridge = object : ArrayBridge<Double> {
-        override val size: Int = array.size
+        override val size: Int = this@asList.size
+
         override fun get(index: Int): Double {
-            return array[index]
+            return this@asList[index]
         }
 
         override fun set(index: Int, element: Double) {
-            array[index] = element
+            this@asList[index] = element
         }
     }
     return ArrayBridgeList(bridge)
