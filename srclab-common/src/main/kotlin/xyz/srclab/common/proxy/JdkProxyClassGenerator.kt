0f6636bdeca1b21aab05689161abc09ace0f1800
SrcLab <fredsuvn@163.com>
2020-11-01 15:23:20 +0800

    Using copy-on-write-way for JdkProxyClassGenerator.

--- srclab-common/src/main/kotlin/xyz/srclab/common/proxy/JdkProxyClassGenerator.kt
+++ srclab-common/src/main/kotlin/xyz/srclab/common/proxy/JdkProxyClassGenerator.kt
@@ -16,13 +16,58 @@ object JdkProxyClassGenerator : ProxyClassGenerator {
     }
 
     private class ProxyMethodInterceptor<T : Any>(val proxyMethods: Iterable<ProxyMethod<T>>) : InvocationHandler {
+
+        private var methodMap: MutableMap<Method, ProxyMethod<T>>? = null
+
         override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any? {
-            val proxyMethod = proxyMethods.find { pm ->
-                method.name == pm.name && method.parameterTypes.contentEquals(pm.parametersTypes)
+            val proxyMethod = findProxyMethod(method)
+            return callProxyMethod(proxyMethod, proxy, method, args)
+        }
+
+        private fun findProxyMethod(method: Method): ProxyMethod<T> {
+            val tryFind = findFromMethodMap(method)
+            if (tryFind !== null) {
+                return tryFind
             }
-            if (proxyMethod === null) {
-                throw IllegalStateException("Proxy method not found: $method")
+
+            synchronized(this) {
+                val proxyMethodFromMapSync = findFromMethodMap(method)
+                if (proxyMethodFromMapSync !== null) {
+                    return proxyMethodFromMapSync
+                }
+                val proxyMethodFromIterable = proxyMethods.find { pm ->
+                    method.name == pm.name && method.parameterTypes.contentEquals(pm.parametersTypes)
+                }
+                if (proxyMethodFromIterable === null) {
+                    throw IllegalStateException("Proxy method not found: $method")
+                }
+
+                //Using copy-on-write-way to update method map.
+                val oldMap = methodMap
+                val newMap = if (oldMap === null) HashMap() else HashMap(oldMap)
+                newMap[method] = proxyMethodFromIterable
+                methodMap = newMap
+                return proxyMethodFromIterable
             }
+        }
+
+        private fun findFromMethodMap(method: Method): ProxyMethod<T>? {
+            val methods = methodMap
+            if (methods !== null) {
+                val proxyMethodFromMap = methods[method]
+                if (proxyMethodFromMap !== null) {
+                    return proxyMethodFromMap
+                }
+            }
+            return null
+        }
+
+        private fun callProxyMethod(
+            proxyMethod: ProxyMethod<T>,
+            proxy: Any,
+            method: Method,
+            args: Array<out Any>?
+        ): Any? {
             return proxyMethod.invoke(proxy.asAny(), method, args, object : SuperInvoker {
                 override fun invoke(args: Array<out Any?>?): Any? {
                     throw IllegalStateException("Illegal method calling: $method")
