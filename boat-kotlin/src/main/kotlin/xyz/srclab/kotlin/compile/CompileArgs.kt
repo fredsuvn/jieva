7d619ec145d2dd596b5d7aa71a8e646b69580086
sunqian <fredsuvn@163.com>
2020-11-04 10:47:01 +0800

    add boat-kotlin

---
+++ boat-kotlin/src/main/kotlin/xyz/srclab/kotlin/compile/CompileArgs.kt
@@ -0,0 +1,5 @@
+@file:JvmName("CompileArgs")
+
+package xyz.srclab.kotlin.compile
+
+const val COMPILE_INAPPLICABLE_JVM_NAME = "INAPPLICABLE_JVM_NAME"
---
+++ boat-kotlin/src/main/kotlin/xyz/srclab/kotlin/jvm/JvmArgs.kt
@@ -0,0 +1,10 @@
+@file:JvmName("JvmArgs")
+
+package xyz.srclab.kotlin.jvm
+
+object UNINITIALIZED_VALUE {
+
+    override fun toString(): String {
+        return "UNINITIALIZED_VALUE"
+    }
+}
