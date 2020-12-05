ffb4ac1fdc2e93b9ba9c098488a14c5c66561a8b
SrcLab <fredsuvn@163.com>
2020-12-05 23:35:19 +0800

    fix invoker

--- boat-common/src/main/kotlin/xyz/srclab/common/base/Invoker.kt
+++ boat-common/src/main/kotlin/xyz/srclab/common/base/Invoker.kt
@@ -83,29 +83,29 @@ object ReflectedInvokerProvider : InvokerProvider {
     override fun forConstructor(constructor: Constructor<*>): Invoker {
         return ReflectedConstructorInvoker(constructor)
     }
-}
 
-private class ReflectedMethodInvoker(private val method: Method) : Invoker {
+    private class ReflectedMethodInvoker(private val method: Method) : Invoker {
 
-    override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
-        return method.invoke(`object`, *args).asAny()
+        override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
+            return method.invoke(`object`, *args).asAny()
+        }
+
+        override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
+            method.isAccessible = true
+            return method.invoke(`object`, *args).asAny()
+        }
     }
 
-    override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
-        method.isAccessible = true
-        return method.invoke(`object`, *args).asAny()
-    }
-}
+    private class ReflectedConstructorInvoker(private val constructor: Constructor<*>) : Invoker {
 
-private class ReflectedConstructorInvoker(private val constructor: Constructor<*>) : Invoker {
+        override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
+            return constructor.newInstance(*args).asAny()
+        }
 
-    override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
-        return constructor.newInstance(*args).asAny()
-    }
-
-    override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
-        constructor.isAccessible = true
-        return constructor.newInstance(*args).asAny()
+        override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
+            constructor.isAccessible = true
+            return constructor.newInstance(*args).asAny()
+        }
     }
 }
 
@@ -118,190 +118,190 @@ object MethodHandlerInvokerProvider : InvokerProvider {
     override fun forConstructor(constructor: Constructor<*>): Invoker {
         return StaticMethodHandlerInvoker(constructor)
     }
-}
 
-private class StaticMethodHandlerInvoker() : Invoker {
+    private class StaticMethodHandlerInvoker() : Invoker {
 
-    private lateinit var methodHandle: MethodHandle
+        private lateinit var methodHandle: MethodHandle
 
-    constructor(method: Method) : this() {
-        methodHandle = findStaticMethodHandle(method)
+        constructor(method: Method) : this() {
+            methodHandle = findStaticMethodHandle(method)
+        }
+
+        constructor(constructor: Constructor<*>) : this() {
+            methodHandle = findStaticMethodHandle(constructor)
+        }
+
+        override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
+            return when (args.size) {
+                0 -> methodHandle.invoke()
+                1 -> methodHandle.invoke(
+                    args[0]
+                )
+                2 -> methodHandle.invoke(
+                    args[0], args[1]
+                )
+                3 -> methodHandle.invoke(
+                    args[0], args[1], args[2]
+                )
+                4 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3]
+                )
+                5 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4]
+                )
+                6 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5]
+                )
+                7 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6]
+                )
+                8 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]
+                )
+                9 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]
+                )
+                10 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
+                )
+                11 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
+                )
+                12 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
+                    args[11],
+                )
+                13 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
+                    args[11], args[12],
+                )
+                14 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
+                    args[11], args[12], args[13],
+                )
+                15 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
+                    args[11], args[12], args[13], args[14],
+                )
+                16 -> methodHandle.invoke(
+                    args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
+                    args[11], args[12], args[13], args[14], args[15],
+                )
+                else -> methodHandle.invokeWithArguments(*args)
+            }.asAny()
+        }
+
+        override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
+            return invoke(`object`, *args)
+        }
+
+        private fun findStaticMethodHandle(method: Method): MethodHandle {
+            //val methodType: MethodType = when (method.parameterCount) {
+            //    0 -> MethodType.methodType(method.returnType)
+            //    else -> MethodType.methodType(method.returnType, method.parameterTypes)
+            //}
+            //return MethodHandles.lookup().findStatic(method.declaringClass, method.name, methodType)
+            method.isAccessible = true
+            return MethodHandles.lookup().unreflect(method)
+        }
+
+        private fun findStaticMethodHandle(constructor: Constructor<*>): MethodHandle {
+            //val methodType: MethodType = when (constructor.parameterCount) {
+            //    0 -> MethodType.methodType(Void::class.javaPrimitiveType)
+            //    else -> MethodType.methodType(Void::class.javaPrimitiveType, constructor.parameterTypes)
+            //}
+            //if (constructor.isPublic) {
+            //    return MethodHandles.lookup().findConstructor(constructor.declaringClass, methodType)
+            //} else {
+            //    constructor.isAccessible = true
+            //    return MethodHandles.lookup().findSpecial(
+            //        constructor.declaringClass, "<init>", methodType, constructor.declaringClass
+            //    )
+            //}
+            constructor.isAccessible = true
+            return MethodHandles.lookup().unreflectConstructor(constructor)
+        }
     }
 
-    constructor(constructor: Constructor<*>) : this() {
-        methodHandle = findStaticMethodHandle(constructor)
-    }
+    private class VirtualMethodHandlerInvoker(method: Method) : Invoker {
 
-    override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
-        return when (args.size) {
-            0 -> methodHandle.invoke()
-            1 -> methodHandle.invoke(
-                args[0]
-            )
-            2 -> methodHandle.invoke(
-                args[0], args[1]
-            )
-            3 -> methodHandle.invoke(
-                args[0], args[1], args[2]
-            )
-            4 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3]
-            )
-            5 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4]
-            )
-            6 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5]
-            )
-            7 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6]
-            )
-            8 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]
-            )
-            9 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]
-            )
-            10 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
-            )
-            11 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
-            )
-            12 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
-                args[11],
-            )
-            13 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
-                args[11], args[12],
-            )
-            14 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
-                args[11], args[12], args[13],
-            )
-            15 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
-                args[11], args[12], args[13], args[14],
-            )
-            16 -> methodHandle.invoke(
-                args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10],
-                args[11], args[12], args[13], args[14], args[15],
-            )
-            else -> methodHandle.invokeWithArguments(*args)
-        }.asAny()
-    }
+        private var methodHandle = findMethodHandle(method)
 
-    override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
-        return invoke(`object`, *args)
-    }
+        override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
+            return when (args.size) {
+                0 -> methodHandle.invoke(
+                    `object`
+                )
+                1 -> methodHandle.invoke(
+                    `object`, args[0]
+                )
+                2 -> methodHandle.invoke(
+                    `object`, args[0], args[1]
+                )
+                3 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2]
+                )
+                4 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3]
+                )
+                5 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4]
+                )
+                6 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5]
+                )
+                7 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6]
+                )
+                8 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]
+                )
+                9 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]
+                )
+                10 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]
+                )
+                11 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
+                    args[10]
+                )
+                12 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
+                    args[10], args[11]
+                )
+                13 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
+                    args[10], args[11], args[12]
+                )
+                14 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
+                    args[10], args[11], args[12], args[13]
+                )
+                15 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
+                    args[10], args[11], args[12], args[13], args[14]
+                )
+                16 -> methodHandle.invoke(
+                    `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
+                    args[10], args[11], args[12], args[13], args[14], args[15]
+                )
+                else -> methodHandle.invokeWithArguments(`object`, *args)
+            }.asAny()
+        }
 
-    private fun findStaticMethodHandle(method: Method): MethodHandle {
-        //val methodType: MethodType = when (method.parameterCount) {
-        //    0 -> MethodType.methodType(method.returnType)
-        //    else -> MethodType.methodType(method.returnType, method.parameterTypes)
-        //}
-        //return MethodHandles.lookup().findStatic(method.declaringClass, method.name, methodType)
-        method.isAccessible = true
-        return MethodHandles.lookup().unreflect(method)
-    }
+        override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
+            return invoke(`object`, *args)
+        }
 
-    private fun findStaticMethodHandle(constructor: Constructor<*>): MethodHandle {
-        //val methodType: MethodType = when (constructor.parameterCount) {
-        //    0 -> MethodType.methodType(Void::class.javaPrimitiveType)
-        //    else -> MethodType.methodType(Void::class.javaPrimitiveType, constructor.parameterTypes)
-        //}
-        //if (constructor.isPublic) {
-        //    return MethodHandles.lookup().findConstructor(constructor.declaringClass, methodType)
-        //} else {
-        //    constructor.isAccessible = true
-        //    return MethodHandles.lookup().findSpecial(
-        //        constructor.declaringClass, "<init>", methodType, constructor.declaringClass
-        //    )
-        //}
-        constructor.isAccessible = true
-        return MethodHandles.lookup().unreflectConstructor(constructor)
-    }
-}
-
-private class VirtualMethodHandlerInvoker(method: Method) : Invoker {
-
-    private var methodHandle = findMethodHandle(method)
-
-    override fun <T> invoke(`object`: Any?, vararg args: Any?): T {
-        return when (args.size) {
-            0 -> methodHandle.invoke(
-                `object`
-            )
-            1 -> methodHandle.invoke(
-                `object`, args[0]
-            )
-            2 -> methodHandle.invoke(
-                `object`, args[0], args[1]
-            )
-            3 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2]
-            )
-            4 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3]
-            )
-            5 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4]
-            )
-            6 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5]
-            )
-            7 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6]
-            )
-            8 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]
-            )
-            9 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]
-            )
-            10 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]
-            )
-            11 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
-                args[10]
-            )
-            12 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
-                args[10], args[11]
-            )
-            13 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
-                args[10], args[11], args[12]
-            )
-            14 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
-                args[10], args[11], args[12], args[13]
-            )
-            15 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
-                args[10], args[11], args[12], args[13], args[14]
-            )
-            16 -> methodHandle.invoke(
-                `object`, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9],
-                args[10], args[11], args[12], args[13], args[14], args[15]
-            )
-            else -> methodHandle.invokeWithArguments(`object`, *args)
-        }.asAny()
-    }
-
-    override fun <T> forceInvoke(`object`: Any?, vararg args: Any?): T {
-        return invoke(`object`, *args)
-    }
-
-    private fun findMethodHandle(method: Method): MethodHandle {
-        //val methodType: MethodType = when (method.parameterCount) {
-        //    0 -> MethodType.methodType(method.returnType)
-        //    else -> MethodType.methodType(method.returnType, method.parameterTypes)
-        //}
-        //return MethodHandles.lookup().findVirtual(method.declaringClass, method.name, methodType)
-        method.isAccessible = true
-        return MethodHandles.lookup().unreflect(method)
+        private fun findMethodHandle(method: Method): MethodHandle {
+            //val methodType: MethodType = when (method.parameterCount) {
+            //    0 -> MethodType.methodType(method.returnType)
+            //    else -> MethodType.methodType(method.returnType, method.parameterTypes)
+            //}
+            //return MethodHandles.lookup().findVirtual(method.declaringClass, method.name, methodType)
+            method.isAccessible = true
+            return MethodHandles.lookup().unreflect(method)
+        }
     }
 }
