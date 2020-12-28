f3421a62c9df6476994553c288193e71328c1209
fredsuvn <fredsuvn@163.com>
2020-12-28 17:20:27 +0800

    add jvm

---
+++ boat-common/src/main/kotlin/xyz/srclab/common/jvm/JvmKit.kt
@@ -0,0 +1,28 @@
+@file:JvmName("JvmKit")
+@file:JvmMultifileClass
+
+package xyz.srclab.common.jvm
+
+import java.lang.reflect.Method
+
+fun Class<*>.toDescriptor(): String {
+    if (this.isArray) {
+        return this.name.replace('.', '/')
+    }
+    return when (this) {
+        Boolean::class.javaPrimitiveType -> "Z"
+        Byte::class.javaPrimitiveType -> "B"
+        Short::class.javaPrimitiveType -> "S"
+        Char::class.javaPrimitiveType -> "C"
+        Int::class.javaPrimitiveType -> "I"
+        Long::class.javaPrimitiveType -> "J"
+        Float::class.javaPrimitiveType -> "F"
+        Double::class.javaPrimitiveType -> "D"
+        Void::class.javaPrimitiveType -> "V"
+        else -> "L${this.name.replace('.', '/')};"
+    }
+}
+
+fun Method.toDescriptor(): String {
+    return "(${this.parameterTypes.joinToString("") { it.toDescriptor() }})${this.returnType.toDescriptor()}"
+}
