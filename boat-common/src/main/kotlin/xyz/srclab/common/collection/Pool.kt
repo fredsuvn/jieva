405bc1469ca8b89fa393722b929e52bac47efb5e
fredsuvn <fredsuvn@163.com>
2021-01-06 16:41:51 +0800

    add Pool

---
+++ boat-common/src/main/kotlin/xyz/srclab/common/collection/Pool.kt
@@ -0,0 +1,29 @@
+package xyz.srclab.common.collection
+
+import java.util.*
+
+/**
+ * Collection contains [Set] and [List].
+ *
+ * @author sunqian
+ */
+interface Pool<E> : Set<E>, List<E> {
+
+    @JvmDefault
+    override fun spliterator(): Spliterator<E> {
+        return super<Set>.spliterator()
+    }
+}
+
+/**
+ * Collection contains [MutableSet] and [MutableList].
+ *
+ * @author sunqian
+ */
+interface MutablePool<E> : Pool<E>, MutableSet<E>, MutableList<E> {
+
+    @JvmDefault
+    override fun spliterator(): Spliterator<E> {
+        return super<Pool>.spliterator()
+    }
+}
---
+++ boat-common/src/main/kotlin/xyz/srclab/common/collection/PoolMap.kt
@@ -0,0 +1,7 @@
+package xyz.srclab.common.collection
+
+/**
+ * @author sunqian
+ */
+interface PoolMap<K, V> : Map<K, V> {
+}
