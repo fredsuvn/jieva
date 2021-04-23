d50408bbf497e95101f1c20fab34b6122a8df9a0
SrcLab <fredsuvn@163.com>
2021-04-24 00:39:38 +0800

    fix enums

--- boat-core/src/test/java/test/java/xyz/srclab/common/base/EnumsTest.java
+++ boat-core/src/test/java/test/java/xyz/srclab/common/base/EnumsTest.java
@@ -8,14 +8,17 @@ public class EnumsTest {
 
     @Test
     public void test() {
-        TestEnum t1 = Enums.valueOf(TestEnum.class, "T1");
+        TestEnum t1 = Enums.value(TestEnum.class, "T1");
         Assert.assertEquals(t1, TestEnum.T1);
-        TestEnum t2 = Enums.valueOfIgnoreCase(TestEnum.class, "t2");
+        TestEnum t2 = Enums.valueIgnoreCase(TestEnum.class, "t2");
         Assert.assertEquals(t2, TestEnum.T2);
+        TestEnum t3 = Enums.value(TestEnum.class, 2);
+        Assert.assertEquals(t3, TestEnum.T3);
     }
 
     public enum TestEnum {
         T1,
-        T2
+        T2,
+        T3
     }
 }
