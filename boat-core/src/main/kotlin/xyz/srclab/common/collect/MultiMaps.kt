e0813a16858fa879a7b283ee3eafca7aaf816c1c
fredsuvn <fredsuvn@163.com>
2021-04-30 14:15:14 +0800

    fix MultiMaps.kt

--- boat-core/src/main/kotlin/xyz/srclab/common/collect/MultiMaps.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/collect/MultiMaps.kt
@@ -1,14 +1,50 @@
-@file:JvmName("MultiMaps")
-
 package xyz.srclab.common.collect
 
 import xyz.srclab.common.base.asAny
+import xyz.srclab.common.collect.ListMap.Companion.toListMap
+import xyz.srclab.common.collect.MutableListMap.Companion.toMutableListMap
+import xyz.srclab.common.collect.MutableSetMap.Companion.toMutableSetMap
+import xyz.srclab.common.collect.SetMap.Companion.toSetMap
 import java.util.*
 
 /**
  * Represents a type of [Map] of which key associated to a [Set].
  */
-interface SetMap<K, V> : Map<K, Set<V>>
+interface SetMap<K, V> : Map<K, Set<V>> {
+
+    fun toMutableSetMap(): MutableSetMap<K, V>
+
+    companion object {
+
+        @JvmName("newSetMap")
+        @JvmStatic
+        fun <K, V> Map<K, Set<V>>.toSetMap(): SetMap<K, V> {
+            return SetMapImpl(this)
+        }
+    }
+
+    private open class SetMapBase<K, V>(map: Map<K, Set<V>>) : Map<K, Set<V>> by map
+    private class SetMapImpl<K, V>(private val map: Map<K, Set<V>>) : SetMapBase<K, V>(map), SetMap<K, V> {
+
+        override fun toMutableSetMap(): MutableSetMap<K, V> {
+            return map.mapTo(LinkedHashMap()) { k, v ->
+                k to LinkedHashSet(v)
+            }.toMutableSetMap()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return map == other
+        }
+
+        override fun hashCode(): Int {
+            return map.hashCode()
+        }
+
+        override fun toString(): String {
+            return map.toString()
+        }
+    }
+}
 
 /**
  * Represents a type of [MutableMap] of which key associated to [MutableSet].
@@ -24,12 +60,103 @@ interface MutableSetMap<K, V> : MutableMap<K, MutableSet<V>> {
      * Add [values] for given [key], return result after adding.
      */
     fun addAll(key: K, values: Iterable<V>): MutableSet<V>
+
+    fun toSetMap(): SetMap<K, V>
+
+    companion object {
+
+        @JvmName("newMutableSetMap")
+        @JvmOverloads
+        @JvmStatic
+        fun <K, V> MutableMap<K, out MutableSet<V>>.toMutableSetMap(
+            setGenerator: () -> MutableSet<V> = { LinkedHashSet() }
+        ): MutableSetMap<K, V> {
+            return MutableSetMapImpl(this.asAny(), setGenerator)
+        }
+
+        @JvmOverloads
+        @JvmStatic
+        fun <K, V> newMutableSetMap(
+            setGenerator: () -> MutableSet<V> = { LinkedHashSet() }
+        ): MutableSetMap<K, V> {
+            return MutableSetMapImpl(LinkedHashMap(), setGenerator)
+        }
+    }
+
+    private open class MutableSetMapBase<K, V>(map: MutableMap<K, MutableSet<V>>) : MutableMap<K, MutableSet<V>> by map
+    private class MutableSetMapImpl<K, V>(
+        private val map: MutableMap<K, MutableSet<V>>,
+        private val setGenerator: () -> MutableSet<V>,
+    ) : MutableSetMapBase<K, V>(map), MutableSetMap<K, V> {
+
+        override fun add(key: K, value: V): MutableSet<V> {
+            val result = this.getOrPut(key, setGenerator)
+            result.add(value)
+            return result
+        }
+
+        override fun addAll(key: K, values: Iterable<V>): MutableSet<V> {
+            val result = this.getOrPut(key, setGenerator)
+            result.addAll(values)
+            return result
+        }
+
+        override fun toSetMap(): SetMap<K, V> {
+            return map.toSetMap()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return map == other
+        }
+
+        override fun hashCode(): Int {
+            return map.hashCode()
+        }
+
+        override fun toString(): String {
+            return map.toString()
+        }
+    }
 }
 
 /**
  * Represents a type of [Map] of which key associated to a [List].
  */
-interface ListMap<K, V> : Map<K, List<V>>
+interface ListMap<K, V> : Map<K, List<V>> {
+
+    fun toMutableListMap(): MutableListMap<K, V>
+
+    companion object {
+
+        @JvmName("newListMap")
+        @JvmStatic
+        fun <K, V> Map<K, List<V>>.toListMap(): ListMap<K, V> {
+            return ListMapImpl(this)
+        }
+    }
+
+    private open class ListMapBase<K, V>(map: Map<K, List<V>>) : Map<K, List<V>> by map
+    private class ListMapImpl<K, V>(private val map: Map<K, List<V>>) : ListMapBase<K, V>(map), ListMap<K, V> {
+
+        override fun toMutableListMap(): MutableListMap<K, V> {
+            return map.mapTo(LinkedHashMap()) { k, v ->
+                k to LinkedList(v)
+            }.toMutableListMap()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return map == other
+        }
+
+        override fun hashCode(): Int {
+            return map.hashCode()
+        }
+
+        override fun toString(): String {
+            return map.toString()
+        }
+    }
+}
 
 /**
  * Represents a type of [MutableMap] of which key associated to [MutableList].
@@ -45,124 +172,63 @@ interface MutableListMap<K, V> : MutableMap<K, MutableList<V>> {
      * Add [values] for given [key], return result after adding.
      */
     fun addAll(key: K, values: Iterable<V>): MutableList<V>
-}
 
-@JvmName("setMap")
-fun <K, V> Map<K, Set<V>>.toSetMap(): SetMap<K, V> {
-    return SetMapImpl(this)
-}
+    fun toListMap(): ListMap<K, V>
 
-@JvmName("mutableSetMap")
-@JvmOverloads
-fun <K, V> MutableMap<K, out MutableSet<V>>.toMutableSetMap(
-    setGenerator: () -> MutableSet<V> = { LinkedHashSet() }
-): MutableSetMap<K, V> {
-    return MutableSetMapImpl(this.asAny(), setGenerator)
-}
+    companion object {
 
-private open class SetMapBase<K, V>(map: Map<K, Set<V>>) : Map<K, Set<V>> by map
-private class SetMapImpl<K, V>(private val map: Map<K, Set<V>>) : SetMapBase<K, V>(map), SetMap<K, V> {
+        @JvmName("newMutableListMap")
+        @JvmOverloads
+        @JvmStatic
+        fun <K, V> MutableMap<K, out MutableList<V>>.toMutableListMap(
+            listGenerator: () -> MutableList<V> = { LinkedList() }
+        ): MutableListMap<K, V> {
+            return MutableListMapImpl(this.asAny(), listGenerator)
+        }
 
-    override fun equals(other: Any?): Boolean {
-        return map == other
+        @JvmOverloads
+        @JvmStatic
+        fun <K, V> newMutableListMap(
+            listGenerator: () -> MutableList<V> = { LinkedList() }
+        ): MutableListMap<K, V> {
+            return MutableListMapImpl(LinkedHashMap(), listGenerator)
+        }
     }
 
-    override fun hashCode(): Int {
-        return map.hashCode()
-    }
+    private open class MutableListMapBase<K, V>(map: MutableMap<K, MutableList<V>>) :
+        MutableMap<K, MutableList<V>> by map
 
-    override fun toString(): String {
-        return map.toString()
-    }
-}
+    private class MutableListMapImpl<K, V>(
+        private val map: MutableMap<K, MutableList<V>>,
+        private val listGenerator: () -> MutableList<V>,
+    ) : MutableListMapBase<K, V>(map), MutableListMap<K, V> {
 
-private open class MutableSetMapBase<K, V>(map: MutableMap<K, MutableSet<V>>) : MutableMap<K, MutableSet<V>> by map
-private class MutableSetMapImpl<K, V>(
-    private val map: MutableMap<K, MutableSet<V>>,
-    private val setGenerator: () -> MutableSet<V>,
-) : MutableSetMapBase<K, V>(map), MutableSetMap<K, V> {
+        override fun add(key: K, value: V): MutableList<V> {
+            val result = this.getOrPut(key, listGenerator)
+            result.add(value)
+            return result
+        }
 
-    override fun add(key: K, value: V): MutableSet<V> {
-        val result = this.getOrPut(key, setGenerator)
-        result.add(value)
-        return result
-    }
+        override fun addAll(key: K, values: Iterable<V>): MutableList<V> {
+            val result = this.getOrPut(key, listGenerator)
+            result.addAll(values)
+            return result
+        }
 
-    override fun addAll(key: K, values: Iterable<V>): MutableSet<V> {
-        val result = this.getOrPut(key, setGenerator)
-        result.addAll(values)
-        return result
-    }
+        override fun toListMap(): ListMap<K, V> {
+            return map.toListMap()
+        }
 
-    override fun equals(other: Any?): Boolean {
-        return map == other
-    }
+        override fun equals(other: Any?): Boolean {
+            return map == other
+        }
 
-    override fun hashCode(): Int {
-        return map.hashCode()
-    }
+        override fun hashCode(): Int {
+            return map.hashCode()
+        }
 
-    override fun toString(): String {
-        return map.toString()
-    }
-}
-
-@JvmName("listMap")
-fun <K, V> Map<K, List<V>>.toListMap(): ListMap<K, V> {
-    return ListMapImpl(this)
-}
-
-@JvmName("mutableListMap")
-@JvmOverloads
-fun <K, V> MutableMap<K, out MutableList<V>>.toMutableListMap(
-    listGenerator: () -> MutableList<V> = { LinkedList() }
-): MutableListMap<K, V> {
-    return MutableListMapImpl(this.asAny(), listGenerator)
-}
-
-private open class ListMapBase<K, V>(map: Map<K, List<V>>) : Map<K, List<V>> by map
-private class ListMapImpl<K, V>(private val map: Map<K, List<V>>) : ListMapBase<K, V>(map), ListMap<K, V> {
-
-    override fun equals(other: Any?): Boolean {
-        return map == other
-    }
-
-    override fun hashCode(): Int {
-        return map.hashCode()
-    }
-
-    override fun toString(): String {
-        return map.toString()
-    }
-}
-
-private open class MutableListMapBase<K, V>(map: MutableMap<K, MutableList<V>>) : MutableMap<K, MutableList<V>> by map
-private class MutableListMapImpl<K, V>(
-    private val map: MutableMap<K, MutableList<V>>,
-    private val listGenerator: () -> MutableList<V>,
-) : MutableListMapBase<K, V>(map), MutableListMap<K, V> {
-
-    override fun add(key: K, value: V): MutableList<V> {
-        val result = this.getOrPut(key, listGenerator)
-        result.add(value)
-        return result
-    }
-
-    override fun addAll(key: K, values: Iterable<V>): MutableList<V> {
-        val result = this.getOrPut(key, listGenerator)
-        result.addAll(values)
-        return result
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return map == other
-    }
-
-    override fun hashCode(): Int {
-        return map.hashCode()
-    }
-
-    override fun toString(): String {
-        return map.toString()
+        override fun toString(): String {
+            return map.toString()
+        }
     }
 }
