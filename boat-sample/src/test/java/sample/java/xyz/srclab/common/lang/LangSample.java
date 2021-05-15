df2ebc4ed12698272804ca59b8464574a79f98f9
SrcLab <fredsuvn@163.com>
2021-05-15 18:19:57 +0800

    rename BaseSample to LangSample

--- boat-sample/src/test/java/sample/java/xyz/srclab/common/lang/LangSample.java
+++ boat-sample/src/test/java/sample/java/xyz/srclab/common/lang/LangSample.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.IntStream;
 
-public class BaseSample {
+public class LangSample {
 
     private static final TestLogger logger = TestLogger.DEFAULT;
 
