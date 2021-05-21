de306c7f436ec1227b671ffd3b0d8824eada7498
SrcLab <fredsuvn@163.com>
2021-05-21 21:27:02 +0800

    fix types

--- boat-core/src/main/kotlin/xyz/srclab/common/collect/Types.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/collect/Types.kt
@@ -3,7 +3,7 @@ package xyz.srclab.common.collect
 import xyz.srclab.annotations.Acceptable
 import xyz.srclab.annotations.Accepted
 import xyz.srclab.common.lang.INAPPLICABLE_JVM_NAME
-import xyz.srclab.common.reflect.parameterizedType
+import xyz.srclab.common.reflect.rawClass
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
@@ -46,36 +46,57 @@ interface IterableType : ParameterizedType {
         @JvmStatic
         @JvmName("from")
         fun Class<*>.toIterableType(): IterableType {
-            return from(this, Any::class.java)
+            return from(this, Any::class.java, this.declaringClass)
         }
 
         @JvmStatic
         @JvmName("from")
         fun ParameterizedType.toIterableType(): IterableType {
-            return IterableTypeImpl(this)
+            return from(this.rawClass, this.actualTypeArguments[0], this.ownerType)
         }
 
+        @JvmOverloads
         @JvmStatic
-        fun from(type: Class<*>, componentType: Type): IterableType {
-            return IterableTypeImpl(parameterizedType(type, componentType))
+        fun from(type: Class<*>, componentType: Type, ownerType: Type? = null): IterableType {
+            return IterableTypeImpl(type, componentType, ownerType)
         }
 
-        private class IterableTypeImpl(private val type: ParameterizedType) : IterableType {
-
-            override val componentType: Type by lazy {
-                this.actualTypeArguments[0]
-            }
+        private class IterableTypeImpl(
+            private val rawType: Class<*>,
+            override val componentType: Type,
+            private val ownerType: Type?,
+        ) : IterableType {
 
             override fun getActualTypeArguments(): Array<Type> {
-                return type.actualTypeArguments
+                return arrayOf(componentType)
             }
 
             override fun getRawType(): Type {
-                return type.rawType
+                return rawType
             }
 
-            override fun getOwnerType(): Type {
-                return type.ownerType
+            override fun getOwnerType(): Type? {
+                return ownerType
+            }
+
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (other !is IterableType) return false
+                if (rawType != other.rawType) return false
+                if (componentType != other.componentType) return false
+                if (ownerType != other.ownerType) return false
+                return true
+            }
+
+            override fun hashCode(): Int {
+                var result = rawType.hashCode()
+                result = 31 * result + componentType.hashCode()
+                result = 31 * result + ownerType.hashCode()
+                return result
+            }
+
+            override fun toString(): String {
+                return "${rawType.typeName}<${componentType.typeName}>"
             }
         }
     }
@@ -118,40 +139,61 @@ interface MapType : ParameterizedType {
         @JvmStatic
         @JvmName("from")
         fun Class<*>.toMapType(): MapType {
-            return from(this, Any::class.java, Any::class.java)
+            return from(this, Any::class.java, Any::class.java, this.declaringClass)
         }
 
         @JvmStatic
         @JvmName("from")
         fun ParameterizedType.toMapType(): MapType {
-            return MapTypeImpl(this)
+            val actualTypeArguments = this.actualTypeArguments
+            return from(this.rawClass, actualTypeArguments[0], actualTypeArguments[1], this.ownerType)
         }
 
+        @JvmOverloads
         @JvmStatic
-        fun from(type: Class<*>, keyType: Type, valueType: Type): MapType {
-            return MapTypeImpl(parameterizedType(type, keyType, valueType))
+        fun from(rawType: Class<*>, keyType: Type, valueType: Type, ownerType: Type? = null): MapType {
+            return MapTypeImpl(rawType, keyType, valueType, ownerType)
         }
 
-        private class MapTypeImpl(private val type: ParameterizedType) : MapType {
-
-            override val keyType: Type by lazy {
-                this.actualTypeArguments[0]
-            }
-
-            override val valueType: Type by lazy {
-                this.actualTypeArguments[1]
-            }
+        private class MapTypeImpl(
+            private val rawType: Class<*>,
+            override val keyType: Type,
+            override val valueType: Type,
+            private val ownerType: Type?,
+        ) : MapType {
 
             override fun getActualTypeArguments(): Array<Type> {
-                return type.actualTypeArguments
+                return arrayOf(keyType, valueType)
             }
 
             override fun getRawType(): Type {
-                return type.rawType
+                return rawType
             }
 
-            override fun getOwnerType(): Type {
-                return type.ownerType
+            override fun getOwnerType(): Type? {
+                return ownerType
+            }
+
+            override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (other !is MapType) return false
+                if (rawType != other.rawType) return false
+                if (keyType != other.keyType) return false
+                if (valueType != other.valueType) return false
+                if (ownerType != other.ownerType) return false
+                return true
+            }
+
+            override fun hashCode(): Int {
+                var result = rawType.hashCode()
+                result = 31 * result + keyType.hashCode()
+                result = 31 * result + valueType.hashCode()
+                result = 31 * result + (ownerType?.hashCode() ?: 0)
+                return result
+            }
+
+            override fun toString(): String {
+                return "${rawType.typeName}<${keyType.typeName},${valueType.typeName}>"
             }
         }
     }
