a054efa35af9286528867d2f37874cdf94075d6a
fredsuvn <fredsuvn@163.com>
2021-10-16 17:48:53 +0800

    todo...

--- boat-codec/src/main/kotlin/xyz/srclab/common/codec/Encoder.kt
+++ boat-codec/src/main/kotlin/xyz/srclab/common/codec/Encoder.kt
@@ -2,6 +2,7 @@ package xyz.srclab.common.codec
 
 import org.bouncycastle.util.encoders.Base64Encoder
 import org.bouncycastle.util.encoders.HexEncoder
+import xyz.srclab.common.base.ByteArrayRef
 import xyz.srclab.common.base.toBytes
 import xyz.srclab.common.base.toChars
 import java.io.ByteArrayOutputStream
@@ -17,55 +18,97 @@ interface Encoder : Codec {
     //encode:
 
     fun encode(data: ByteArray): ByteArray {
-        return encode(data, 0)
-    }
-
-    fun encode(data: ByteArray, offset: Int): ByteArray {
-        return encode(data, offset, data.size - offset)
-    }
-
-    fun encode(data: ByteArray, offset: Int, length: Int): ByteArray {
         val outputStream = ByteArrayOutputStream()
-        encode(data, offset, length, outputStream)
+        encode(data, outputStream)
         return outputStream.toByteArray()
+    }
+
+    fun encode(data: ByteArrayRef): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        encode(data, outputStream)
+        return outputStream.toByteArray()
+    }
+
+    fun encode(data: InputStream): ByteArray {
+        return encode(data.readBytes())
+    }
+
+    fun encode(data:CharSequence):ByteArray {
+        return encode(data.toBytes())
+    }
+
+    fun encode(data:Reader):ByteArray {
+        return encode(data.readText())
+    }
+
+    fun encode(date: ByteArray, output: ByteArray):Int {
+        return encode(date,0,date.size,output,0)
+    }
+
+    fun encode(date: ByteArrayRef, output: ByteArray):Int {
+        return encode(date.array,date.offset,date.length,output,0)
+    }
+
+    fun encode(date: InputStream, output: ByteArray):Int {
+        return encode(date.readBytes(),output)
+    }
+
+    fun encode(date: CharSequence, output: ByteArray):Int {
+        return encode(date.toBytes(),output)
+    }
+
+    fun encode(date: Reader, output: ByteArray):Int {
+        return encode(date.readText(),output)
+    }
+
+    fun encode(date: ByteArrayRef, output: ByteArrayRef):Int {
+        return encode(date.array,date.offset,date.length,output.array,output.offset,output.length)
+    }
+
+    fun encode(date: ByteArrayRef, output: ByteArrayRef):Int {
+        return encode(date.array,date.offset,date.length,output.array,output.offset,output.length)
+    }
+
+    fun encode(date: ByteArrayRef, output: ByteArrayRef):Int {
+        return encode(date.array,date.offset,date.length,output.array,output.offset,output.length)
+    }
+
+    fun encode(date: ByteArrayRef, output: ByteArrayRef):Int {
+        return encode(date.array,date.offset,date.length,output.array,output.offset,output.length)
+    }
+
+    fun encode(date: ByteArrayRef, output: ByteArrayRef):Int {
+        return encode(date.array,date.offset,date.length,output.array,output.offset,output.length)
     }
 
     fun encode(data: ByteArray, output: OutputStream): Int {
-        return encode(data, 0, output)
+        return encode(data,0,data.size,output)
     }
 
-    fun encode(data: ByteArray, offset: Int, output: OutputStream): Int {
-        return encode(data, offset, data.size - offset, output)
+    fun encode(data: ByteArrayRef, output: OutputStream): Int{
+        return encode(data.array,data.offset,data.length,output)
     }
 
-    fun encode(data: ByteArray, offset: Int, length: Int, output: OutputStream): Int
+    fun encode(
+        data: ByteArray, dataOffset: Int, dataLength: Int,
+        output: ByteArray, outputOffset: Int
+    ):Int
 
-    fun encode(data: InputStream, output: OutputStream): Int {
-        return encode(data.readBytes(), output)
-    }
+    fun encode(
+        data: ByteArray, dataOffset: Int, dataLength: Int,
+        output: OutputStream
+    ):Int
 
-    fun encode(data: CharSequence): ByteArray {
-        val outputStream = ByteArrayOutputStream()
-        encode(data.toBytes(), outputStream)
-        return outputStream.toByteArray()
-    }
+    fun encode(data: InputStream, output: OutputStream):Int
 
-    fun encode(data: CharSequence, output: OutputStream): Int
 
-    fun encode(data: Reader, output: OutputStream): Int {
-        return encode(data.readText(), output)
-    }
 
     fun encodeToString(data: ByteArray): String {
         return encode(data).toChars()
     }
 
-    fun encodeToString(data: ByteArray, offset: Int): String {
-        return encode(data, offset).toChars()
-    }
-
-    fun encodeToString(data: ByteArray, offset: Int, length: Int): String {
-        return encode(data, offset, length).toChars()
+    fun encodeToString(data: ByteArrayRef): String {
+        return encode(data).toChars()
     }
 
     fun encodeToString(data: CharSequence): String {
@@ -75,28 +118,20 @@ interface Encoder : Codec {
     //decode:
 
     fun decode(data: ByteArray): ByteArray {
-        return decode(data, 0)
+        return decode(ByteArrayRef.of(data))
     }
 
-    fun decode(data: ByteArray, offset: Int): ByteArray {
-        return decode(data, offset, data.size - offset)
-    }
-
-    fun decode(data: ByteArray, offset: Int, length: Int): ByteArray {
+    fun decode(data: ByteArrayRef): ByteArray {
         val outputStream = ByteArrayOutputStream()
-        decode(data, offset, length, outputStream)
+        decode(data, outputStream)
         return outputStream.toByteArray()
     }
 
     fun decode(data: ByteArray, output: OutputStream): Int {
-        return decode(data, 0, output)
+        return decode(ByteArrayRef.of(data), output)
     }
 
-    fun decode(data: ByteArray, offset: Int, output: OutputStream): Int {
-        return decode(data, offset, data.size - offset, output)
-    }
-
-    fun decode(data: ByteArray, offset: Int, length: Int, output: OutputStream): Int
+    fun decode(data: ByteArrayRef, output: OutputStream): Int
 
     fun decode(data: InputStream, output: OutputStream): Int {
         return decode(data.readBytes(), output)
@@ -118,12 +153,8 @@ interface Encoder : Codec {
         return decode(data).toChars()
     }
 
-    fun decodeToString(data: ByteArray, offset: Int): String {
-        return decode(data, offset).toChars()
-    }
-
-    fun decodeToString(data: ByteArray, offset: Int, length: Int): String {
-        return decode(data, offset, length).toChars()
+    fun decodeToString(data: ByteArrayRef): String {
+        return decode(data).toChars()
     }
 
     fun decodeToString(data: CharSequence): String {
@@ -152,7 +183,7 @@ interface Encoder : Codec {
             override val algorithm: String
         ) : Encoder {
 
-            override fun encode(data: ByteArray, offset: Int, length: Int, output: OutputStream): Int {
+            override fun encode(data: ByteArrayRef, output: OutputStream): Int {
                 return encoder.encode(data, offset, length, output)
             }
 
@@ -160,7 +191,7 @@ interface Encoder : Codec {
                 return encode(data.toBytes(), output)
             }
 
-            override fun decode(data: ByteArray, offset: Int, length: Int, output: OutputStream): Int {
+            override fun decode(data: ByteArrayRef, output: OutputStream): Int {
                 return encoder.decode(data, offset, length, output)
             }
 
