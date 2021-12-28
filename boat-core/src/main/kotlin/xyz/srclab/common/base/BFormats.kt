4ee124571ff9cfaf9f55ee72e2782fa0fb288b17
fredsuvn <fredsuvn@163.com>
2021-12-28 14:40:06 +0800

    update BFormats

--- boat-core/src/main/kotlin/xyz/srclab/common/base/BFormats.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/base/BFormats.kt
@@ -2,77 +2,112 @@
 
 package xyz.srclab.common.base
 
-import org.slf4j.helpers.MessageFormatter as MessageFormatterSlf4j
-import java.text.MessageFormat as MessageFormatKt
+import xyz.srclab.common.base.StringRef.Companion.stringRef
+import java.util.*
 
 /**
- * Formats by [FastCharsFormat].
+ * Fast format using slf4j-like style (may not exactly same):
+ *
+ * ```
+ * BFormats.fastFormat("1 + 1 = {}, 2 + 2 = {}", 2, 4);
+ * ```
+ *
+ * Using `\` as escape in following cases:
+ *
+ * * `\{}` will be escaped as `{}`;
+ * * `\\` will be escaped as `\`;
+ * * Otherwise no escape;
+ *
+ * For example:
+ *
+ * ```
+ * //Note java string `\\` means `\`
+ * BFormats.fastFormat("C:\\\\{}\\_\\{}", "file.zip");
+ * ```
+ *
+ * will output:
+ *
+ * ```
+ * C:\file.zip\_{}
+ * ```
  */
 fun CharSequence.fastFormat(vararg args: Any?): String {
-    return BFormat.FAST_FORMAT.format(this, *args)
-}
+    //return MessageFormatterSlf4j.arrayFormat(this.toString(), args, null).message
 
-/**
- * Formats by [PrintfCharsFormat].
- */
-fun CharSequence.printfFormat(vararg args: Any?): String {
-    return BFormat.PRINTF_FORMAT.format(this, *args)
-}
+    var buffer: MutableList<Any?>? = null
 
-/**
- * Formats by [MessageCharsFormat].
- */
-fun CharSequence.messageFormat(vararg args: Any?): String {
-    return BFormat.MESSAGE_FORMAT.format(this, *args)
-}
-
-/**
- * Format for [CharSequence].
- *
- * @see FastCharsFormat
- * @see PrintfCharsFormat
- * @see MessageCharsFormat
- */
-interface BFormat {
-
-    fun format(pattern: CharSequence, vararg args: Any?): String
-
-    companion object {
-
-        @JvmField
-        val FAST_FORMAT: FastCharsFormat = FastCharsFormat
-
-        @JvmField
-        val PRINTF_FORMAT: PrintfCharsFormat = PrintfCharsFormat
-
-        @JvmField
-        val MESSAGE_FORMAT: MessageCharsFormat = MessageCharsFormat
+    fun getBuffer(): MutableList<Any?> {
+        val bf = buffer
+        if (bf === null) {
+            val newBuffer = LinkedList<Any?>()
+            buffer = newBuffer
+            return newBuffer
+        }
+        return bf
     }
-}
 
-/**
- * [BFormat] with `slf4j` style.
- */
-object FastCharsFormat : BFormat {
-    override fun format(pattern: CharSequence, vararg args: Any?): String {
-        return MessageFormatterSlf4j.arrayFormat(pattern.toString(), args, null).message
+    var start = 0
+    var i = 0
+    var argIndex = 0
+    while (i < this.length) {
+        val c = this[i]
+        if (c == '\\') {
+            i++
+            if (i < this.length) {
+                val cn = this[i]
+                if (cn == '\\') {
+                    //Escape: \\ -> \
+                    getBuffer().add(this.stringRef(start, i))
+                    i++
+                    start = i
+                    continue
+                } else if (cn == '{') {
+                    i++
+                    if (i < this.length) {
+                        val cnn = this[i]
+                        if (cnn == '}') {
+                            //Escape: \{} -> {}
+                            getBuffer().add(this.stringRef(start, i - 2))
+                            start = i - 1
+                            i++
+                            continue
+                        }
+                    } else {
+                        break
+                    }
+                }
+            } else {
+                break
+            }
+        } else if (c == '{') {
+            i++
+            if (i < this.length) {
+                val cn = this[i]
+                if (cn == '}') {
+                    //Insert parameter
+                    if (!argIndex.isIndexInBounds(0, args.size)) {
+                        throw IndexOutOfBoundsException("Argument index out of bounds: $argIndex")
+                    }
+                    getBuffer().add(this.stringRef(start, i - 1))
+                    getBuffer().add(args[argIndex])
+                    i++
+                    start = i
+                    argIndex++
+                    continue
+                }
+            } else {
+                break
+            }
+        }
+        i++
     }
-}
 
-/**
- * [BFormat] with `System.out.printf` style.
- */
-object PrintfCharsFormat : BFormat {
-    override fun format(pattern: CharSequence, vararg args: Any?): String {
-        return String.format(pattern.toString(), *args)
+    if (buffer === null) {
+        return this.toString()
     }
-}
+    if (start < this.length) {
+        getBuffer().add(this.stringRef(start))
+    }
 
-/**
- * [BFormat] with `MessageFormat` style.
- */
-object MessageCharsFormat : BFormat {
-    override fun format(pattern: CharSequence, vararg args: Any?): String {
-        return MessageFormatKt.format(pattern.toString(), *args)
-    }
+    return getBuffer().joinToString("")
 }
