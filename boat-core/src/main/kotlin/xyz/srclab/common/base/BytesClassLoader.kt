e78fba7e2174fc4d987b9224a23a30cea8b67e5d
fredsuvn <fredsuvn@163.com>
2021-12-29 14:35:34 +0800

    add BytesClassLoader

---
+++ boat-core/src/main/kotlin/xyz/srclab/common/base/BytesClassLoader.kt
@@ -0,0 +1,36 @@
+package xyz.srclab.common.base
+
+import java.io.InputStream
+import java.nio.ByteBuffer
+
+/**
+ * [ClassLoader] used to help load class from bytes.
+ */
+object BytesClassLoader : ClassLoader() {
+
+    @JvmOverloads
+    fun loadClass(bytes: ByteArray, offset: Int = 0, length: Int = bytes.size - offset): Class<*> {
+        return super.defineClass(null, bytes, offset, length)
+    }
+
+    @JvmOverloads
+    fun loadClass(name: String, bytes: ByteArray, offset: Int = 0, length: Int = bytes.size - offset): Class<*> {
+        return super.defineClass(name, bytes, offset, length)
+    }
+
+    fun loadClass(inputStream: InputStream): Class<*> {
+        return loadClass(inputStream.readBytes())
+    }
+
+    fun loadClass(name: String, inputStream: InputStream): Class<*> {
+        return loadClass(name, inputStream.readBytes())
+    }
+
+    fun loadClass(byteBuffer: ByteBuffer): Class<*> {
+        return super.defineClass(null, byteBuffer, null)
+    }
+
+    fun loadClass(name: String, byteBuffer: ByteBuffer): Class<*> {
+        return super.defineClass(name, byteBuffer, null)
+    }
+}
