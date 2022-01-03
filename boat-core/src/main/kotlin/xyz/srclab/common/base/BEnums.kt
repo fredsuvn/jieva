8a37f4e6ba36c2b38d659e8b2ca1a99044f70e26
SrcLab <fredsuvn@163.com>
2022-01-03 14:27:43 +0800

    update BEnums

--- boat-core/src/main/kotlin/xyz/srclab/common/base/BEnums.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/base/BEnums.kt
@@ -2,56 +2,57 @@
 
 package xyz.srclab.common.base
 
-@Throws(IllegalArgumentException::class)
 @JvmName("getValue")
-fun <T> Class<*>.enumValue(name: CharSequence): T {
-    return enumValueOrNull(name) ?: throw IllegalArgumentException("Value of $this not found: $name")
+@Throws(EnumNotFoundException::class)
+fun <T : Enum<T>> Class<*>.enumValue(name: CharSequence, ignoreCase: Boolean = false): T {
+    return enumValueOrNull(name, ignoreCase) ?: throw EnumNotFoundException("$this.$name")
 }
 
-@Throws(IllegalArgumentException::class)
 @JvmName("getValueOrNull")
-fun <T> Class<*>.enumValueOrNull(name: CharSequence): T? {
+fun <T> Class<*>.enumValueOrNull(name: CharSequence, ignoreCase: Boolean = false): T? {
+    if (!ignoreCase) {
+        return name.toString().toEnumOrNull(this)?.asTyped()
+    }
     return try {
-        JavaEnum.valueOf(this.asTyped<Class<Enum<*>>>(), name.toString()).asTyped()
+        val values: Array<out Enum<*>>? = this.enumConstants?.asTyped()
+        if (values.isNullOrEmpty()) {
+            return null
+        }
+        for (value in values) {
+            if (value.name.contentEquals(name, true)) {
+                return value.asTyped()
+            }
+        }
+        null
     } catch (e: Exception) {
         null
     }
 }
 
-@Throws(IllegalArgumentException::class)
-@JvmName("getValueIgnoreCase")
-fun <T> Class<*>.enumValueIgnoreCase(name: CharSequence): T {
-    return enumValueIgnoreCaseOrNull(name) ?: throw IllegalArgumentException("Value of $this not found: $name")
-}
-
-@Throws(IllegalArgumentException::class)
-@JvmName("getValueIgnoreCaseOrNull")
-fun <T> Class<*>.enumValueIgnoreCaseOrNull(name: CharSequence): T? {
-    val t = enumValueOrNull<T>(name)
-    if (t !== null) {
-        return t
-    }
-    val values: Array<out Enum<*>> =
-        this.enumConstants.asTyped() ?: throw IllegalArgumentException("Must be an enum type: $this")
-    for (value in values) {
-        if (value.name.contentEquals(name, true)) {
-            return value.asTyped()
-        }
-    }
-    return null
-}
-
-@Throws(IndexOutOfBoundsException::class)
 @JvmName("getValue")
+@Throws(EnumNotFoundException::class)
 fun <T> Class<*>.enumValue(index: Int): T {
-    return enumValueOrNull(index) ?: throw IllegalArgumentException("Value of $this not found: index $index")
+    return enumValueOrNull(index) ?: throw EnumNotFoundException("$this[$index]")
 }
 
 @JvmName("getValueOrNull")
 fun <T> Class<*>.enumValueOrNull(index: Int): T? {
-    val values = this.enumConstants ?: throw IllegalArgumentException("Must be an enum type: $this")
+    val values = this.enumConstants
+    if (values.isNullOrEmpty()) {
+        return null
+    }
     if (index.isIndexInBounds(0, values.size)) {
         return values[index].asTyped()
     }
     return null
 }
+
+private fun String.toEnumOrNull(type: Class<*>): Any? {
+    return try {
+        java.lang.Enum.valueOf(type.asTyped<Class<Enum<*>>>(), this)
+    } catch (e: Exception) {
+        null
+    }
+}
+
+open class EnumNotFoundException(message: String?) : RuntimeException(message)
