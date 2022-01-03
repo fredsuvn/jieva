a2b16f163d063fafb00d722fcf8b73e6c6144cf5
SrcLab <fredsuvn@163.com>
2022-01-03 15:16:44 +0800

    update RunContext

--- boat-core/src/main/kotlin/xyz/srclab/common/run/RunContext.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/run/RunContext.kt
@@ -7,12 +7,20 @@ import xyz.srclab.common.base.asTyped
  */
 interface RunContext {
 
-    fun get(key: Any): Any? {
+    fun get(key: Any): Any {
+        return asMap()[key] ?: NoSuchElementException("$key")
+    }
+
+    fun getOrNull(key: Any): Any? {
         return asMap()[key]
     }
 
-    fun <T> getTyped(key: Any): T {
+    fun <T : Any> getTyped(key: Any): T {
         return get(key).asTyped()
+    }
+
+    fun <T : Any> getTypedOrNull(key: Any): T? {
+        return getOrNull(key).asTyped()
     }
 
     fun set(key: Any, value: Any?) {
