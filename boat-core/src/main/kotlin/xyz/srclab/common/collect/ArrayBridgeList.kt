017cc59398b9128072209202ccf60c21839948b8
fredsuvn <fredsuvn@163.com>
2022-01-04 15:59:56 +0800

    update array

---
+++ boat-core/src/main/kotlin/xyz/srclab/common/collect/ArrayBridgeList.kt
@@ -0,0 +1,40 @@
+package xyz.srclab.common.collect
+
+import java.io.Serializable
+
+/**
+ * Bridge between array and [MutableList].
+ */
+class ArrayBridgeList<T>(
+    private val arrayBridge: ArrayBridge<T>,
+) : AbstractMutableList<T>(), RandomAccess, Serializable {
+
+    override val size: Int get() = arrayBridge.size
+
+    override fun isEmpty(): Boolean = arrayBridge.isEmpty()
+    override fun contains(element: T): Boolean = arrayBridge.contains(element)
+    override fun get(index: Int): T = arrayBridge[index]
+    override fun set(index: Int, element: T): T = arrayBridge[index].let { arrayBridge[index] = element;it }
+    override fun indexOf(element: T): Int = arrayBridge.indexOf(element)
+    override fun lastIndexOf(element: T): Int = arrayBridge.lastIndexOf(element)
+    override fun add(index: Int, element: T) = throw UnsupportedOperationException()
+    override fun removeAt(index: Int): T = throw UnsupportedOperationException()
+
+    companion object {
+        private const val serialVersionUID = -2764017481108945198L
+    }
+}
+
+/**
+ * Provides base info for [ArrayBridgeList].
+ */
+// Fuck un-supporting-primitive-generic-type! DAMN!
+interface ArrayBridge<T> {
+    val size: Int
+    fun isEmpty(): Boolean
+    fun contains(element: T): Boolean
+    operator fun get(index: Int): T
+    operator fun set(index: Int, element: T): T
+    fun indexOf(element: T): Int
+    fun lastIndexOf(element: T): Int
+}
---
+++ boat-core/src/main/kotlin/xyz/srclab/common/collect/BArray.kt
@@ -0,0 +1,284 @@
+@file:JvmName("BArray")
+
+package xyz.srclab.common.collect
+
+import xyz.srclab.common.base.asTyped
+import xyz.srclab.common.reflect.rawClass
+import java.lang.reflect.Type
+import java.util.*
+import kotlin.collections.joinTo as joinToKt
+import kotlin.collections.joinToString as joinToStringKt
+
+private const val NOT_ARRAY_TYPE_PREFIX = "Not an array type"
+
+fun <T> Type.newArray(length: Int): Array<T> {
+    return java.lang.reflect.Array.newInstance(this.rawClass, length).asTyped()
+}
+
+/**
+ * Returns a fixed-size array associated given array,
+ * or throws [IllegalArgumentException] if given object is not an array.
+ */
+@JvmName("asList")
+fun <T> Any.arrayAsList(): MutableList<T> {
+    return when (this) {
+        is Array<*> -> this.asList().asTyped()
+        is BooleanArray -> this.asList().asTyped()
+        is ByteArray -> this.asList().asTyped()
+        is ShortArray -> this.asList().asTyped()
+        is CharArray -> this.asList().asTyped()
+        is IntArray -> this.asList().asTyped()
+        is LongArray -> this.asList().asTyped()
+        is FloatArray -> this.asList().asTyped()
+        is DoubleArray -> this.asList().asTyped()
+        else -> throw IllegalArgumentException("$NOT_ARRAY_TYPE_PREFIX: ${this.javaClass}")
+    }
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun <T> Array<T>.asList(): MutableList<T> {
+    return Arrays.asList(*this)
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun ByteArray.asList(): MutableList<Byte> {
+    return ArrayBridgeList(object : ArrayBridge<Byte> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Byte): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Byte = this@asList[index]
+        override fun set(index: Int, element: Byte): Byte = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Byte): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Byte): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun ShortArray.asList(): MutableList<Short> {
+    return ArrayBridgeList(object : ArrayBridge<Short> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Short): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Short = this@asList[index]
+        override fun set(index: Int, element: Short): Short = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Short): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Short): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun IntArray.asList(): MutableList<Int> {
+    return ArrayBridgeList(object : ArrayBridge<Int> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Int): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Int = this@asList[index]
+        override fun set(index: Int, element: Int): Int = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Int): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Int): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun LongArray.asList(): MutableList<Long> {
+    return ArrayBridgeList(object : ArrayBridge<Long> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Long): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Long = this@asList[index]
+        override fun set(index: Int, element: Long): Long = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Long): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Long): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun FloatArray.asList(): MutableList<Float> {
+    return ArrayBridgeList(object : ArrayBridge<Float> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Float): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Float = this@asList[index]
+        override fun set(index: Int, element: Float): Float = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Float): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Float): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun DoubleArray.asList(): MutableList<Double> {
+    return ArrayBridgeList(object : ArrayBridge<Double> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Double): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Double = this@asList[index]
+        override fun set(index: Int, element: Double): Double = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Double): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Double): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun BooleanArray.asList(): MutableList<Boolean> {
+    return ArrayBridgeList(object : ArrayBridge<Boolean> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Boolean): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Boolean = this@asList[index]
+        override fun set(index: Int, element: Boolean): Boolean = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Boolean): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Boolean): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+/**
+ * Returns a [MutableList] that wraps the original array.
+ */
+fun CharArray.asList(): MutableList<Char> {
+    return ArrayBridgeList(object : ArrayBridge<Char> {
+        override val size: Int get() = this@asList.size
+        override fun isEmpty(): Boolean = this@asList.isEmpty()
+        override fun contains(element: Char): Boolean = this@asList.contains(element)
+        override fun get(index: Int): Char = this@asList[index]
+        override fun set(index: Int, element: Char): Char = this@asList[index].let { this@asList[index] = element;it }
+        override fun indexOf(element: Char): Int = this@asList.indexOf(element)
+        override fun lastIndexOf(element: Char): Int = this@asList.lastIndexOf(element)
+    })
+}
+
+@JvmOverloads
+@JvmName("joinToString")
+fun Any.arrayJoinToString(
+    separator: CharSequence = ", ",
+    transform: ((Any?) -> CharSequence)? = null
+): String {
+    return this.arrayJoinToString0(separator = separator, transform = transform)
+}
+
+@JvmName("joinToString")
+fun Any.arrayJoinToString(
+    separator: CharSequence = ", ",
+    limit: Int = -1,
+    truncated: CharSequence = "...",
+    transform: ((Any?) -> CharSequence)? = null
+): String {
+    return this.arrayJoinToString0(
+        separator = separator,
+        limit = limit,
+        truncated = truncated,
+        transform = transform,
+    )
+}
+
+@JvmName("joinToString")
+fun Any.arrayJoinToString(
+    separator: CharSequence = ", ",
+    prefix: CharSequence = "",
+    suffix: CharSequence = "",
+    limit: Int = -1,
+    truncated: CharSequence = "...",
+    transform: ((Any?) -> CharSequence)? = null
+): String {
+    return this.arrayJoinToString0(separator, prefix, suffix, limit, truncated, transform)
+}
+
+private fun Any.arrayJoinToString0(
+    separator: CharSequence = ", ",
+    prefix: CharSequence = "",
+    suffix: CharSequence = "",
+    limit: Int = -1,
+    truncated: CharSequence = "...",
+    transform: ((Any?) -> CharSequence)? = null
+): String {
+    return when (this) {
+        is Array<*> -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is BooleanArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is ByteArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is ShortArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is CharArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is IntArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is LongArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is FloatArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        is DoubleArray -> joinToStringKt(separator, prefix, suffix, limit, truncated, transform)
+        else -> throw IllegalArgumentException("$NOT_ARRAY_TYPE_PREFIX: ${this.javaClass}")
+    }
+}
+
+@JvmOverloads
+@JvmName("joinTo")
+fun <A : Appendable> Any.arrayJoinTo(
+    dest: A,
+    separator: CharSequence = ", ",
+    transform: ((Any?) -> CharSequence)? = null
+): A {
+    return this.arrayJoinTo0(dest = dest, separator = separator, transform = transform)
+}
+
+@JvmName("joinTo")
+fun <A : Appendable> Any.arrayJoinTo(
+    dest: A,
+    separator: CharSequence = ", ",
+    limit: Int = -1,
+    truncated: CharSequence = "...",
+    transform: ((Any?) -> CharSequence)? = null
+): A {
+    return this.arrayJoinTo0(
+        dest = dest,
+        separator = separator,
+        limit = limit,
+        truncated = truncated,
+        transform = transform
+    )
+}
+
+@JvmName("joinTo")
+fun <A : Appendable> Any.arrayJoinTo(
+    dest: A,
+    separator: CharSequence = ", ",
+    prefix: CharSequence = "",
+    suffix: CharSequence = "",
+    limit: Int = -1,
+    truncated: CharSequence = "...",
+    transform: ((Any?) -> CharSequence)? = null
+): A {
+    return this.arrayJoinTo0(dest, separator, prefix, suffix, limit, truncated, transform)
+}
+
+private fun <A : Appendable> Any.arrayJoinTo0(
+    dest: A,
+    separator: CharSequence = ", ",
+    prefix: CharSequence = "",
+    suffix: CharSequence = "",
+    limit: Int = -1,
+    truncated: CharSequence = "...",
+    transform: ((Any?) -> CharSequence)? = null
+): A {
+    return when (this) {
+        is Array<*> -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is BooleanArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is ByteArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is ShortArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is CharArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is IntArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is LongArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is FloatArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        is DoubleArray -> joinToKt(dest, separator, prefix, suffix, limit, truncated, transform)
+        else -> throw IllegalArgumentException("$NOT_ARRAY_TYPE_PREFIX: ${this.javaClass}")
+    }
+}
