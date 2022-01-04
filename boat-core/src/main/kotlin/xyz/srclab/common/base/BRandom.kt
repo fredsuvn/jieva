68367baa1360f3e65ceea420ec20ae430e8a716d
fredsuvn <fredsuvn@163.com>
2022-01-04 12:44:37 +0800

    update BRandom

--- boat-core/src/main/kotlin/xyz/srclab/common/base/BRandom.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/base/BRandom.kt
@@ -23,79 +23,65 @@ fun Random.between(from: Int, to: Int): Int {
 }
 
 /**
- * Returns a builder for [Randomer].
+ * Returns a new [RandomSupplierBuilder].
  */
-fun <T : Any> randomerBuilder(): Randomer.Builder<T> {
-    return Randomer.Builder()
+fun <T : Any> randomSupplierBuilder(): RandomSupplierBuilder<T> {
+    return RandomSupplierBuilder()
 }
 
-/**
- * Random object generator.
- */
-interface Randomer<T : Any> {
 
-    fun next(): T
+class RandomSupplierBuilder<T : Any> {
 
-    class Builder<T : Any> {
+    private var builderRandom: Random? = null
+    private val builderCases: MutableList<Case<T>> = LinkedList()
+    private var scoreCount = 1
 
-        private var builderRandom: Random? = null
-        private val builderCases: MutableList<Case<T>> = LinkedList()
-        private var scoreCount = 1
+    fun score(score: Int, obj: T): RandomSupplierBuilder<T> {
+        return score(score) { obj }
+    }
 
-        fun score(score: Int, obj: T): Builder<T> {
-            return score(score) { obj }
-        }
+    @JvmSynthetic
+    fun score(score: Int, supplier: () -> T): RandomSupplierBuilder<T> {
+        return score(score, supplier.toSupplier())
+    }
 
-        @JvmSynthetic
-        fun score(score: Int, supplier: () -> T): Builder<T> {
-            return score(score, supplier.toSupplier())
-        }
+    fun score(score: Int, supplier: Supplier<T>): RandomSupplierBuilder<T> {
+        builderCases.add(Case(scoreCount, scoreCount + score, supplier))
+        scoreCount += score
+        return this
+    }
 
-        fun score(score: Int, supplier: Supplier<T>): Builder<T> {
-            builderCases.add(Case(scoreCount, scoreCount + score, supplier))
-            scoreCount += score
-            return this
-        }
+    fun random(random: Random): RandomSupplierBuilder<T> {
+        this.builderRandom = random
+        return this
+    }
 
-        fun random(random: Random): Builder<T> {
-            this.builderRandom = random
-            return this
-        }
+    fun build(): Supplier<T> {
+        return object : Supplier<T> {
 
-        fun build(): Randomer<T> {
-            return object : Randomer<T> {
+            private val random = builderRandom ?: Random()
+            private val totalScore = scoreCount
+            private val cases = builderCases.sortedWith { a, b -> a.from - b.from }
 
-                private val random = builderRandom ?: Random()
-                private val totalScore = scoreCount
-                private val cases = builderCases.sortedWith { a, b -> a.from - b.from }
-
-                override fun next(): T {
-                    val score = random.between(1, totalScore)
-                    val index = cases.binarySearch {
-                        if (it.from <= score && it.to > score) {
-                            return@binarySearch EQUAL_TO
-                        }
-                        if (it.from > score) {
-                            return@binarySearch GREATER_THAN
-                        }
-                        LESS_THAN
+            override fun get(): T {
+                val score = random.between(1, totalScore)
+                val index = cases.binarySearch {
+                    if (it.from <= score && it.to > score) {
+                        return@binarySearch EQUAL_TO
                     }
-                    return cases[index].supplier.get()
+                    if (it.from > score) {
+                        return@binarySearch GREATER_THAN
+                    }
+                    LESS_THAN
                 }
+                return cases[index].supplier.get()
             }
         }
-
-        private data class Case<T>(
-            val from: Int,
-            val to: Int,
-            val supplier: Supplier<T>
-        )
     }
 
-    companion object {
-        @JvmStatic
-        fun <T : Any> newBuilder(): Builder<T> {
-            return Builder()
-        }
-    }
+    private data class Case<T>(
+        val from: Int,
+        val to: Int,
+        val supplier: Supplier<T>
+    )
 }
