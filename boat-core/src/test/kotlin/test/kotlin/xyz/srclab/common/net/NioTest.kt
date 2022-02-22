346d2502bbcbfc801f92499e0ecd561402002a44
fredsuvn <fredsuvn@163.com>
2022-02-22 16:47:52 +0800

    add NioTest

--- boat-core/src/test/kotlin/test/kotlin/xyz/srclab/common/net/NioTest.kt
+++ boat-core/src/test/kotlin/test/kotlin/xyz/srclab/common/net/NioTest.kt
@@ -3,6 +3,7 @@ package test.kotlin.xyz.srclab.common.net
 import org.testng.annotations.Test
 import xyz.srclab.common.base.getBytes
 import xyz.srclab.common.base.sleep
+import xyz.srclab.common.io.getString
 import xyz.srclab.common.net.socket.availableLocalhost
 import xyz.srclab.common.run.runAsync
 import java.net.SocketAddress
@@ -34,6 +35,7 @@ class NioTest {
         serverSocket.configureBlocking(false)
         serverSocket.bind(address)
         serverSocket.register(selector, SelectionKey.OP_ACCEPT)
+        val map: MutableMap<SocketChannel, Int> = HashMap()
         while (true) {
             val count = selector.select()
             if (count > 0) {
@@ -49,15 +51,51 @@ class NioTest {
                         client.configureBlocking(false)
                         println(">>> Acceptable client: $client(${System.identityHashCode(client)})")
                         client.register(selector, SelectionKey.OP_READ)
+                        map.compute(client) { _, c ->
+                            val result =
+                                if (c === null) {
+                                    1
+                                } else {
+                                    c + 1
+                                }
+                            println(">>> Acceptable client count: $result")
+                            if (result == 1) {
+                                runAsync { readClient(client) }
+                            }
+                            result
+                        }
                     }
                     if (next.isReadable) {
                         val client = next.channel() as SocketChannel
                         println(">>> Readable: $client(${System.identityHashCode(client)})")
+                        map.compute(client) { _, c ->
+                            val result =
+                                if (c === null) {
+                                    1
+                                } else {
+                                    c + 1
+                                }
+                            println(">>> Readable client count: $result")
+                            if (result == 1) {
+                                runAsync { readClient(client) }
+                            }
+                            result
+                        }
                         //channel.register(selector, SelectionKey.OP_READ)
                     }
                 }
             }
             sleep(1000)
+        }
+    }
+
+    private fun readClient(channel: SocketChannel) {
+        while (true) {
+            val buffer = ByteBuffer.allocate(100)
+            channel.read(buffer)
+            buffer.flip()
+            println("read: ${buffer.getString()}")
+            sleep(5000)
         }
     }
 }
