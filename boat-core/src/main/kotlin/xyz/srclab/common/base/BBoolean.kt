ee40a9dbcfe752578f84fe6e2201b8ce6fddd6cf
SrcLab <fredsuvn@163.com>
2022-03-05 20:45:25 +0800

    update BBoolean

--- boat-core/src/main/kotlin/xyz/srclab/common/base/BBoolean.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/base/BBoolean.kt
@@ -2,94 +2,77 @@
 
 package xyz.srclab.common.base
 
-fun CharSequence?.toBoolean(): Boolean {
-    return this.contentEquals("true", true)
-}
-
+/**
+ * If [this] is boolean type ,return itself;
+ * if null, return false;
+ * else calls the [toCharSeq] then return whether the charSeq equals to the string "true", case ignored.
+ */
 fun Any?.toBoolean(): Boolean {
     return when (this) {
         null -> false
         is Boolean -> this
-        else -> this.toCharSeq().toBoolean()
+        else -> this.toCharSeq().contentEquals("true", true)
     }
 }
 
-fun anyTrue(vararg charSeqs: CharSequence?): Boolean {
-    for (charSeq in charSeqs) {
-        if (charSeq.toBoolean()) {
+/**
+ * Returns true if any of the result of [toBoolean] for each [objs] is true.
+ */
+fun anyTrue(vararg objs: Any?): Boolean {
+    for (obj in objs) {
+        if (obj.toBoolean()) {
             return true
         }
     }
     return false
 }
 
-fun anyTrue(charSeqs: Iterable<CharSequence?>): Boolean {
-    for (charSeq in charSeqs) {
-        if (charSeq.toBoolean()) {
+/**
+ * Returns true if all the result of [toBoolean] for each [objs] is true.
+ */
+fun allTrue(vararg objs: Any?): Boolean {
+    for (obj in objs) {
+        if (!obj.toBoolean()) {
+            return false
+        }
+    }
+    return true
+}
+
+/**
+ * Returns true if any of the result of [toBoolean] for each [objs] is false.
+ */
+fun anyFalse(vararg objs: Any?): Boolean {
+    for (obj in objs) {
+        if (!obj.toBoolean()) {
             return true
         }
     }
     return false
 }
 
-fun allTrue(vararg charSeqs: CharSequence?): Boolean {
-    for (charSeq in charSeqs) {
-        if (!charSeq.toBoolean()) {
+/**
+ * Returns true if all the result of [toBoolean] for each [objs] is false.
+ */
+fun allFalse(vararg objs: Any?): Boolean {
+    for (obj in objs) {
+        if (obj.toBoolean()) {
             return false
         }
     }
     return true
 }
 
-fun allTrue(charSeqs: Iterable<CharSequence?>): Boolean {
-    for (charSeq in charSeqs) {
-        if (!charSeq.toBoolean()) {
-            return false
-        }
-    }
-    return true
-}
-
-fun anyFalse(vararg charSeqs: CharSequence?): Boolean {
-    for (charSeq in charSeqs) {
-        if (!charSeq.toBoolean()) {
-            return true
-        }
-    }
-    return false
-}
-
-fun anyFalse(charSeqs: Iterable<CharSequence?>): Boolean {
-    for (charSeq in charSeqs) {
-        if (!charSeq.toBoolean()) {
-            return true
-        }
-    }
-    return false
-}
-
-fun allFalse(vararg charSeqs: CharSequence?): Boolean {
-    for (charSeq in charSeqs) {
-        if (charSeq.toBoolean()) {
-            return false
-        }
-    }
-    return true
-}
-
-fun allFalse(charSeqs: Iterable<CharSequence?>): Boolean {
-    for (charSeq in charSeqs) {
-        if (charSeq.toBoolean()) {
-            return false
-        }
-    }
-    return true
-}
-
+/**
+ * Returns 1 if [this] is true, else 0.
+ */
 fun Boolean.toInt(): Int {
     return if (this) 1 else 0
 }
 
+/**
+ * Returns 1 if [this] is not-null and true, else 0.
+ */
 fun Boolean?.toInt(): Int {
     return if (this !== null && this) 1 else 0
 }
