9cdbe938b1ed27220cf40b4cd63c050daef0b808
fredsuvn <fredsuvn@163.com>
2022-03-25 17:26:53 +0800

    cache todo...

--- boat-core/src/main/kotlin/xyz/srclab/common/cache/GuavaCache.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/cache/GuavaCache.kt
@@ -1,68 +1,75 @@
 package xyz.srclab.common.cache
 
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
 import com.google.common.cache.RemovalCause
+import com.google.common.cache.RemovalListener
 import xyz.srclab.common.base.Val
+import xyz.srclab.common.base.Val.Companion.toVal
+import xyz.srclab.common.base.asTyped
+import java.util.concurrent.ExecutionException
 import java.util.function.Function
 
-open class GuavaCacheImpl<K : Any, V>(builder: Cache.Builder<K, V>) : Cache<K, V> {
+open class GuavaCache<K : Any, V>(builder: Cache.Builder<K, V>) : Cache<K, V> {
 
-    protected val cache:com.google.common.cache.Cache<Any, Any> = run {
-        val guavaBuilder = com.google.common.cache.CacheBuilder.newBuilder()
-        if (builder.initialCapacity !== null) {
-            guavaBuilder.initialCapacity(builder.initialCapacity)
-        }
-        if (params.maxCapacity !== null) {
-            guavaBuilder.maximumSize(params.maxCapacity)
-        }
-        if (params.concurrencyLevel !== null) {
-            guavaBuilder.concurrencyLevel(params.concurrencyLevel)
-        }
-        if (params.expireAfterAccess !== null) {
-            guavaBuilder.expireAfterAccess(params.expireAfterAccess)
-        }
-        if (params.expireAfterWrite !== null) {
-            guavaBuilder.expireAfterWrite(params.expireAfterWrite)
-        }
-        if (params.refreshAfterWrite !== null) {
-            guavaBuilder.refreshAfterWrite(params.refreshAfterWrite)
-        }
-        if (params.listener !== null) {
-            guavaBuilder.removalListener(RemovalListener<K, V> { notification ->
-                val removeCause: Cache.RemoveCause = when (notification.cause!!) {
-                    RemovalCause.EXPLICIT -> Cache.RemoveCause.EXPLICIT
-                    RemovalCause.REPLACED -> Cache.RemoveCause.REPLACED
-                    RemovalCause.COLLECTED -> Cache.RemoveCause.COLLECTED
-                    RemovalCause.EXPIRED -> Cache.RemoveCause.EXPIRED
-                    RemovalCause.SIZE -> Cache.RemoveCause.SIZE
-                }
-                params.listener.afterRemove(notification.key, notification.value, removeCause)
-            })
-        }
-        val loader = params.loader
-        return if (loader === null) {
-            val guavaCache = guavaBuilder.build<K, V>()
-            GuavaCache(guavaCache)
+    protected val cache: com.google.common.cache.Cache<Any, Any> = run {
+        val guavaBuilder = buildGuavaBuilder(builder)
+        val loader = builder.loader
+        if (loader === null) {
+            guavaBuilder.build()
         } else {
-            val loadingGuavaCache =
-                guavaBuilder.build(object : com.google.common.cache.CacheLoader<K, V>() {
-                    override fun load(key: K): V {
-                        val wrapper = loader.load(key)
-                    }
-                })
-            GuavaLoadingCache(loadingGuavaCache)
+            guavaBuilder.build(buildGuavaLoader(NULL, loader))
         }
     }
 
     override fun get(key: K): V {
-        TODO("Not yet implemented")
+        val result = cache.getIfPresent(key)
+        if (result === null) {
+            throw NoSuchElementException(key.toString())
+        }
+        return if (result === NULL) {
+            null.asTyped()
+        } else {
+            result.asTyped()
+        }
     }
 
     override fun get(key: K, loader: Function<in K, Val<V>?>): V {
-        TODO("Not yet implemented")
+        val result = try {
+            cache.get(key) {
+                val result = loader.apply(key)
+                if (result === null) {
+                    throw GuavaLoadingFailedException()
+                }
+                result.value ?: NULL
+            }
+        } catch (e: ExecutionException) {
+            val cause = e.cause
+            if (cause === null || cause !is GuavaLoadingFailedException) {
+                throw e
+            }
+            NOT_FOUND
+        }
+        if (result === NOT_FOUND) {
+            throw NoSuchElementException(key.toString())
+        }
+        return if (result === NULL) {
+            null.asTyped()
+        } else {
+            result.asTyped()
+        }
     }
 
     override fun getVal(key: K): Val<V>? {
-        TODO("Not yet implemented")
+        val result = cache.getIfPresent(key)
+        if (result === null) {
+            return null
+        }
+        return if (result === NULL) {
+            null.toVal().asTyped()
+        } else {
+            result.toVal().asTyped()
+        }
     }
 
     override fun getVal(key: K, loader: Function<in K, Val<V>?>): Val<V>? {
@@ -117,12 +124,16 @@ open class GuavaCacheImpl<K : Any, V>(builder: Cache.Builder<K, V>) : Cache<K, V
         TODO("Not yet implemented")
     }
 
+    companion object {
+        private const val NULL = "GuavaCache.NULL"
+        private const val NOT_FOUND = "GuavaCache.NOT_FOUND"
+    }
 }
 
 /**
  * [Cache] based on [google guava](https://github.com/google/guava).
  */
-open class GuavaCache<K : Any, V : Any>(
+open class GuavaCache2<K : Any, V : Any>(
     private val guava: com.google.common.cache.Cache<K, V>
 ) : Cache<K, V> {
 
@@ -172,6 +183,67 @@ open class GuavaLoadingCache<K : Any, V : Any>(
         return guava.get(key)
     }
 }
+
+private fun <K : Any, V> buildGuavaBuilder(builder: Cache.Builder<K, V>): CacheBuilder<Any, Any> {
+    val guavaBuilder = CacheBuilder.newBuilder()
+    val initialCapacity = builder.initialCapacity
+    if (initialCapacity !== null) {
+        guavaBuilder.initialCapacity(initialCapacity)
+    }
+    val maxCapacity = builder.maxCapacity
+    if (maxCapacity !== null) {
+        guavaBuilder.maximumSize(maxCapacity)
+    }
+    val concurrencyLevel = builder.concurrencyLevel
+    if (concurrencyLevel !== null) {
+        guavaBuilder.concurrencyLevel(concurrencyLevel)
+    }
+    val expireAfterAccess = builder.expireAfterAccess
+    if (expireAfterAccess !== null) {
+        guavaBuilder.expireAfterAccess(expireAfterAccess)
+    }
+    val expireAfterWrite = builder.expireAfterWrite
+    if (expireAfterWrite !== null) {
+        guavaBuilder.expireAfterWrite(expireAfterWrite)
+    }
+    val refreshAfterWrite = builder.refreshAfterWrite
+    if (refreshAfterWrite !== null) {
+        guavaBuilder.refreshAfterWrite(refreshAfterWrite)
+    }
+    val listener = builder.listener
+    if (listener !== null) {
+        guavaBuilder.removalListener(RemovalListener<Any, Any> { notification ->
+            val removeCause: Cache.RemoveCause = when (notification.cause!!) {
+                RemovalCause.EXPLICIT -> Cache.RemoveCause.EXPLICIT
+                RemovalCause.REPLACED -> Cache.RemoveCause.REPLACED
+                RemovalCause.COLLECTED -> Cache.RemoveCause.COLLECTED
+                RemovalCause.EXPIRED -> Cache.RemoveCause.EXPIRED
+                RemovalCause.SIZE -> Cache.RemoveCause.SIZE
+            }
+            listener.afterRemove(notification.key.asTyped(), notification.value.asTyped(), removeCause)
+        })
+    }
+    return guavaBuilder
+}
+
+private fun <K : Any, V> buildGuavaLoader(nullValue: Any, loader: Cache.Loader<K, V>): CacheLoader<Any, Any> {
+    return object : CacheLoader<Any, Any>() {
+
+        override fun load(key: Any): Any {
+            val result = loader.load(key.asTyped())
+            if (result === null) {
+                throw Exception(GuavaLoadingFailedException())
+            }
+            return result.value ?: nullValue
+        }
+
+        override fun loadAll(keys: Iterable<Any>): Map<Any, Any> {
+            return loader.loadAll(keys.asTyped()).asTyped()
+        }
+    }
+}
+
+private class GuavaLoadingFailedException : RuntimeException()
 
 
 
