6da2a4069a18d4d577a5cc7526490b417e3c02c7
fredsuvn <fredsuvn@163.com>
2022-07-05 00:25:11 +0800

    update EventBus.kt

--- boat-core/src/main/kotlin/xyz/srclab/common/base/EventBus.kt
+++ boat-core/src/main/kotlin/xyz/srclab/common/base/EventBus.kt
@@ -3,6 +3,7 @@ package xyz.srclab.common.base
 import xyz.srclab.common.run.SyncRunner
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executor
+import java.util.function.BiConsumer
 import java.util.function.Consumer
 
 /**
@@ -13,10 +14,23 @@ interface EventBus {
     /**
      * Adds and replaces listener on [channel].
      *
+     * @param T type of event
      * @param channel event type
      * @param handler event handler
      */
     fun <T> on(channel: Any, handler: Consumer<in T>)
+
+    /**
+     * Adds and replaces listener on [channel].
+     *
+     * This method can share the context through current thread and execution thread of [handler].
+     *
+     *@param T type of event
+     * @param C type of context
+     * @param channel event type
+     * @param handler event handler
+     */
+    fun <C, T> on(channel: Any, handler: BiConsumer<in T, in C>)
 
     /**
      * Removes listener on [channel].
@@ -24,9 +38,14 @@ interface EventBus {
     fun off(channel: Any)
 
     /**
-     * Emits event on [channel].
+     * Emits [event] on [channel].
      */
     fun emit(channel: Any, event: Any)
+
+    /**
+     * Emits [event] and [context] on [channel].
+     */
+    fun <C> emit(channel: Any, event: Any, context: C)
 
     companion object {
 
@@ -40,17 +59,21 @@ interface EventBus {
         @JvmOverloads
         fun newEventBus(
             executor: Executor = SyncRunner,
-            handlerMap: MutableMap<Any, Consumer<*>> = ConcurrentHashMap()
+            handlerMap: MutableMap<Any, Any> = ConcurrentHashMap()
         ): EventBus {
             return EventBusImpl(executor, handlerMap)
         }
 
         private class EventBusImpl(
             private val executor: Executor,
-            private val handlerMap: MutableMap<Any, Consumer<*>>,
+            private val handlerMap: MutableMap<Any, Any>,
         ) : EventBus {
 
             override fun <T> on(channel: Any, handler: Consumer<in T>) {
+                handlerMap[channel] = handler
+            }
+
+            override fun <C, T> on(channel: Any, handler: BiConsumer<in T, in C>) {
                 handlerMap[channel] = handler
             }
 
@@ -59,11 +82,19 @@ interface EventBus {
             }
 
             override fun emit(channel: Any, event: Any) {
-                val handler: Consumer<Any>? = handlerMap[channel].asType()
-                if (handler === null) {
+                val handler = handlerMap[channel]
+                if (handler === null || handler !is Consumer<*>) {
                     return
                 }
-                executor.execute { handler.accept(event) }
+                executor.execute { handler.asType<Consumer<Any?>>().accept(event) }
+            }
+
+            override fun <C> emit(channel: Any, event: Any, context: C) {
+                val handler = handlerMap[channel]
+                if (handler === null || handler !is BiConsumer<*, *>) {
+                    return
+                }
+                executor.execute { handler.asType<BiConsumer<Any?, C>>().accept(event, context) }
             }
         }
     }
