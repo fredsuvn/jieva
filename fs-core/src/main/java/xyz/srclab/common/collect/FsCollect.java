97d7ba5ddb3a29b7171be81e17d74c540df9ed39
fredsuvn <fredsuvn@163.com>
2023-04-14 23:26:33 +0800

    FsCollect

--- fs-core/src/main/java/xyz/srclab/common/collect/FsCollect.java
+++ fs-core/src/main/java/xyz/srclab/common/collect/FsCollect.java
@@ -3,8 +3,8 @@ package xyz.srclab.common.collect;
 import xyz.srclab.build.annotations.FsMethods;
 import xyz.srclab.common.base.FsArray;
 
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Collection utilities.
@@ -15,14 +15,56 @@ import java.util.LinkedList;
 public class FsCollect {
 
     /**
-     * Puts all elements from given iterable into dest collection, and returns the dest collection.
+     * Puts all given elements into dest collection, and returns the dest collection.
      *
-     * @param iterable given iterable
      * @param dest     dest collection
+     * @param elements given elements
      */
-    public static <T, C extends Collection<? super T>> C toCollection(Iterable<T> iterable, C dest) {
-        for (T t : iterable) {
+    @SafeVarargs
+    public static <T, C extends Collection<? super T>> C toCollection(C dest, T... elements) {
+        dest.addAll(Arrays.asList(elements));
+        return dest;
+    }
+
+    /**
+     * Puts all given elements into dest collection, and returns the dest collection.
+     *
+     * @param dest     dest collection
+     * @param elements given elements
+     */
+    public static <T, C extends Collection<? super T>> C toCollection(C dest, Iterable<T> elements) {
+        for (T t : elements) {
             dest.add(t);
+        }
+        return dest;
+    }
+
+    /**
+     * Puts all given key-values into dest map, and returns the dest map.
+     * <p>
+     * The key-values is an array of which elements are in order of key followed by value,
+     * means the first element (index 0) is key, second (index 1) is value, third is key, fourth is value and so on.
+     * If the array miss the last value, the last value will be considered as null.
+     *
+     * @param dest      dest collection
+     * @param keyValues given key-values
+     */
+    public static <K, V, M extends Map<? super K, ? super V>> M toMap(M dest, Object... keyValues) {
+        int count = 0;
+        K key = null;
+        V value;
+        for (Object keyValue : keyValues) {
+            if (count == 0) {
+                key = (K) keyValue;
+                count++;
+            } else {
+                value = (V) keyValue;
+                dest.put(key, value);
+                count = 0;
+            }
+        }
+        if (count > 0) {
+            dest.put(key, null);
         }
         return dest;
     }
@@ -42,8 +84,67 @@ public class FsCollect {
             }
             return array;
         } else {
-            LinkedList<T> list = toCollection(iterable, new LinkedList<>());
+            LinkedList<T> list = toCollection(new LinkedList<>(), iterable);
             return toArray(list, type);
         }
+    }
+
+    /**
+     * Creates an {@link ArrayList} and put all given elements into it.
+     *
+     * @param elements given elements
+     */
+    @SafeVarargs
+    public static <T> ArrayList<T> arrayList(T... elements) {
+        return toCollection(new ArrayList<>(elements.length), elements);
+    }
+
+    /**
+     * Creates an {@link LinkedList} and put all given elements into it.
+     *
+     * @param elements given elements
+     */
+    @SafeVarargs
+    public static <T> LinkedList<T> linkedList(T... elements) {
+        return new LinkedList<>(Arrays.asList(elements));
+    }
+
+    /**
+     * Creates an {@link HashMap} and put all given key-values into it.
+     * <p>
+     * The key-values is an array of which elements are in order of key followed by value,
+     * means the first element (index 0) is key, second (index 1) is value, third is key, fourth is value and so on.
+     * If the array miss the last value, the last value will be considered as null.
+     *
+     * @param keyValues given key-values
+     */
+    public static <K, V> HashMap<K, V> hashMap(Object... keyValues) {
+        return toMap(new HashMap<>(keyValues.length / 2 + 1), keyValues);
+    }
+
+    /**
+     * Creates an {@link LinkedHashMap} and put all given key-values into it.
+     * <p>
+     * The key-values is an array of which elements are in order of key followed by value,
+     * means the first element (index 0) is key, second (index 1) is value, third is key, fourth is value and so on.
+     * If the array miss the last value, the last value will be considered as null.
+     *
+     * @param keyValues given key-values
+     */
+    public static <K, V> LinkedHashMap<K, V> linkedHashMap(Object... keyValues) {
+        return toMap(new LinkedHashMap<>(keyValues.length / 2 + 1), keyValues);
+    }
+
+    /**
+     * Creates an {@link ConcurrentHashMap} and put all given key-values into it.
+     * <p>
+     * The key-values is an array of which elements are in order of key followed by value,
+     * means the first element (index 0) is key, second (index 1) is value, third is key, fourth is value and so on.
+     * If the array miss the last value, the last value will be considered as null.
+     *
+     * @param keyValues given key-values
+     */
+    public static <K, V> ConcurrentHashMap<K, V> concurrentHashMap(Object... keyValues) {
+        return toMap(new ConcurrentHashMap<>(keyValues.length / 2 + 1), keyValues);
     }
 }
