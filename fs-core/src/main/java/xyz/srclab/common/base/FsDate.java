ec609b218a73a18398a8cee69749126e9c2af783
fredsuvn <fredsuvn@163.com>
2023-06-13 20:03:10 +0800

    FsDate

--- fs-core/src/main/java/xyz/srclab/common/base/FsDate.java
+++ fs-core/src/main/java/xyz/srclab/common/base/FsDate.java
@@ -1,9 +1,64 @@
 package xyz.srclab.common.base;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 /**
  * Date utilities.
  *
  * @author fredsuvn
  */
 public class FsDate {
+
+    /**
+     * Formats given date with given pattern.
+     *
+     * @param date    given date
+     * @param pattern given pattern
+     */
+    public static String format(Date date, String pattern) {
+        return new SimpleDateFormat(pattern).format(date);
+    }
+
+    /**
+     * Formats given date with {@link FsProps#DATE_PATTERN}.
+     *
+     * @param date given date
+     */
+    public static String format(Date date) {
+        return format(date, FsProps.DATE_PATTERN);
+    }
+
+    /**
+     * Parses given date string with given pattern.
+     *
+     * @param date    given date string
+     * @param pattern given pattern
+     */
+    public static Date parse(String date, String pattern) {
+        try {
+            return new SimpleDateFormat(pattern).parse(date);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Parses given date string with {@link FsProps#DATE_PATTERN}.
+     *
+     * @param date given date string
+     */
+    public static Date parse(String date) {
+        return parse(date, FsProps.DATE_PATTERN);
+    }
+
+    /**
+     * Returns current zone offset.
+     */
+    public static ZoneOffset currentZoneOffset() {
+        return ZonedDateTime.now().getOffset();
+    }
 }
