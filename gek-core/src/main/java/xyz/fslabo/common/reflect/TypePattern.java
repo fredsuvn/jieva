02cb1867f73476882178658f3ae8585cf70d5bda
fredsuvn <fredsuvn@163.com>
2024-07-09 16:39:32 +0800

    optimize TypePattern

--- gek-core/src/main/java/xyz/fslabo/common/reflect/TypePattern.java
+++ gek-core/src/main/java/xyz/fslabo/common/reflect/TypePattern.java
@@ -242,164 +242,199 @@ final class TypePattern {
         if (Objects.equals(assigned, assignee) || Objects.equals(assigned, Object.class)) {
             return true;
         }
-        if ((assigned instanceof WildcardType)
-            || (assignee instanceof WildcardType)) {
-            return false;
-        }
-        if (assigned instanceof Class<?>) {
-            Class<?> c1 = (Class<?>) assigned;
-            if (assignee instanceof Class<?>) {
-                Class<?> c2 = (Class<?>) assignee;
-                return isAssignable2(c1, c2);
-            }
-            if (assignee instanceof ParameterizedType) {
-                ParameterizedType p2 = (ParameterizedType) assignee;
-                Class<?> r2 = getRawType(p2);
-                if (r2 == null) {
-                    return false;
-                }
-                return isAssignable2(c1, r2);
-            }
-            if (assignee instanceof TypeVariable<?>) {
-                TypeVariable<?> v2 = (TypeVariable<?>) assignee;
-                Type[] bounds = v2.getBounds();
-                for (Type bound : bounds) {
-                    if (isAssignable(assigned, bound)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-            if (assignee instanceof GenericArrayType) {
-                if (!c1.isArray()) {
-                    return false;
-                }
-                GenericArrayType g2 = (GenericArrayType) assignee;
-                Class<?> gc2 = getRawType(g2.getGenericComponentType());
-                if (gc2 == null) {
-                    return false;
-                }
-                return isAssignable2(c1.getComponentType(), gc2);
-            }
-            return false;
-        }
-        if (assigned instanceof ParameterizedType) {
-            ParameterizedType p1 = (ParameterizedType) assigned;
-            if (assignee instanceof Class<?>) {
-                ParameterizedType p2 = getGenericSuperType(assignee, (Class<?>) p1.getRawType());
-                if (p2 == null) {
-                    return false;
-                }
-                return isAssignable(p1, p2);
-            }
-            if (assignee instanceof ParameterizedType) {
-                ParameterizedType p2 = (ParameterizedType) assignee;
-                if (Objects.equals(p1.getRawType(), p2.getRawType())) {
-                    Type[] a1 = p1.getActualTypeArguments();
-                    Type[] a2 = p2.getActualTypeArguments();
-                    if (a1 == null || a2 == null || a1.length != a2.length) {
-                        return false;
-                    }
-                    for (int i = 0; i < a1.length; i++) {
-                        Type at1 = a1[i];
-                        Type at2 = a2[i];
-                        if (!isAssignableFromForParameterizedType(at1, at2)) {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-                ParameterizedType sp2 = getGenericSuperType(p2, (Class<?>) p1.getRawType());
-                if (sp2 == null) {
-                    return false;
-                }
-                return isAssignable(p1, sp2);
-            }
-            if (assignee instanceof TypeVariable<?>) {
-                TypeVariable<?> v2 = (TypeVariable<?>) assignee;
-                Type[] bounds = v2.getBounds();
-                for (Type bound : bounds) {
-                    if (isAssignable(assigned, bound)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-            return false;
-        }
-        if (assigned instanceof TypeVariable<?>) {
-            TypeVariable<?> v1 = (TypeVariable<?>) assigned;
-            if (assignee instanceof TypeVariable<?>) {
-                TypeVariable<?> v2 = (TypeVariable<?>) assignee;
-                Type[] bounds = v2.getBounds();
-                for (Type bound : bounds) {
-                    if (isAssignable(assigned, bound)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }
-        if (assigned instanceof GenericArrayType) {
-            GenericArrayType g1 = (GenericArrayType) assigned;
-            if (assignee instanceof Class<?>) {
-                Class<?> c2 = (Class<?>) assignee;
-                if (!c2.isArray()) {
-                    return false;
-                }
-                Class<?> tc1 = getRawType(g1.getGenericComponentType());
-                if (tc1 == null) {
-                    return false;
-                }
-                Class<?> tc2 = c2.getComponentType();
-                return isAssignable2(tc1, tc2);
-            }
-            if (assignee instanceof GenericArrayType) {
-                GenericArrayType g2 = (GenericArrayType) assignee;
-                Type tc1 = g1.getGenericComponentType();
-                Type tc2 = g2.getGenericComponentType();
-                return isAssignable(tc1, tc2);
-            }
-            return false;
-        }
-        return false;
+         if (assigned instanceof Class<?>) {
+             Class<?> p = (Class<?>) assigned;
+             if (assignee instanceof Class<?>) {
+                 Class<?> m = (Class<?>) assignee;
+                 return p.isAssignableFrom(m);
+             }
+             if (assignee instanceof ParameterizedType) {
+                 ParameterizedType m = (ParameterizedType) assignee;
+                 Class<?> mRaw = (Class<?>) m.getRawType();
+                 //Note:
+                 // List list = null;
+                 // List<? extends String> list2 = null;
+                 // list = list2;// true!
+                 // list2 = list;// true!
+                 return p.isAssignableFrom(mRaw);
+             }
+             if (assignee instanceof TypeVariable<?>) {
+                 return matches(JieType.wildcard(p, null), assigned);
+             }
+             if (assignee instanceof WildcardType) {
+                 return matches(JieType.wildcard(p, null), assigned);
+             }
+             if (assignee instanceof GenericArrayType) {
+                 GenericArrayType m = (GenericArrayType) assignee;
+                 if (!p.isArray()) {
+                     return false;
+                 }
+                 Type mComponent = m.getGenericComponentType();
+                 Type pComponent = p.getComponentType();
+                 return matches(pComponent, mComponent);
+             }
+             return false;
+         }
+         if (assigned instanceof ParameterizedType) {
+             ParameterizedType p = (ParameterizedType) assigned;
+             if (assignee instanceof Class<?>) {
+                 Class<?> m = (Class<?>) assignee;
+                 Class<?> pRaw = (Class<?>) p.getRawType();
+                 if (!Objects.equals(pRaw, m)) {
+                     return false;
+                 }
+                 Type[] args = p.getActualTypeArguments();
+                 return matchesTypeArgs(args, EMPTY_TYPE_ARRAY);
+             }
+             if (assignee instanceof ParameterizedType) {
+                 ParameterizedType m = (ParameterizedType) assignee;
+                 Class<?> pRaw = (Class<?>) p.getRawType();
+                 Class<?> mRaw = (Class<?>) m.getRawType();
+                 if (!Objects.equals(pRaw, mRaw)) {
+                     return false;
+                 }
+                 Type[] pArgs = Jie.orDefault(p.getActualTypeArguments(), EMPTY_TYPE_ARRAY);
+                 Type[] mArgs = Jie.orDefault(m.getActualTypeArguments(), EMPTY_TYPE_ARRAY);
+                 return matchesTypeArgs(pArgs, mArgs);
+             }
+             if (assignee instanceof TypeVariable<?>) {
+                 return false;
+             }
+             if (assignee instanceof WildcardType) {
+                 return matchesTypeWildcard(assigned, (WildcardType) assignee);
+             }
+             if (assignee instanceof GenericArrayType) {
+                 return false;
+             }
+             return false;
+         }
+         if (assigned instanceof TypeVariable<?>) {
+             TypeVariable<?> p = (TypeVariable<?>) assigned;
+             if (assignee instanceof Class<?>) {
+                 return matchesTypeVariable(p, assignee);
+             }
+             if (assignee instanceof ParameterizedType) {
+                 return matchesTypeVariable(p, assignee);
+             }
+             if (assignee instanceof TypeVariable<?>) {
+                 TypeVariable<?> m = (TypeVariable<?>) assignee;
+                 Type[] pBounds = JieReflect.getUpperBounds(p);
+                 Type[] mBounds = JieReflect.getUpperBounds(m);
+                 for (Type pu : pBounds) {
+                     boolean ok = false;
+                     for (Type mu : mBounds) {
+                         if (isAssignable(pu, mu)) {
+                             ok = true;
+                             break;
+                         }
+                     }
+                     if (!ok) {
+                         return false;
+                     }
+                 }
+                 return true;
+             }
+             if (assignee instanceof WildcardType) {
+                 WildcardType m = (WildcardType) assignee;
+                 Type mUpper = JieReflect.getUpperBound(m);
+                 Type[] pUpper = JieReflect.getUpperBounds(p);
+                 for (Type pu : pUpper) {
+                     if (!isAssignable(pu, mUpper)) {
+                         return false;
+                     }
+                 }
+                 return true;
+             }
+             if (assignee instanceof GenericArrayType) {
+                 return matchesTypeVariable(p, assignee);
+             }
+             return false;
+         }
+         if (assigned instanceof WildcardType) {
+             WildcardType p = (WildcardType) assigned;
+             if (assignee instanceof Class<?>) {
+                 return matchesWildcardType(p, assignee);
+             }
+             if (assignee instanceof ParameterizedType) {
+                 return matchesWildcardType(p, assignee);
+             }
+             if (assignee instanceof TypeVariable<?>) {
+                 TypeVariable<?> m = (TypeVariable<?>) assignee;
+                 Type pLower = JieReflect.getLowerBound(p);
+                 if (pLower != null) {
+                     return false;
+                 }
+                 Type[] mUppers = JieReflect.getUpperBounds(m);
+                 Type pUpper = JieReflect.getUpperBound(p);
+                 for (Type mu : mUppers) {
+                     if (isAssignable(pUpper, mu)) {
+                         return true;
+                     }
+                 }
+                 return false;
+             }
+             if (assignee instanceof WildcardType) {
+                 WildcardType m = (WildcardType) assignee;
+                 Type pUpper = JieReflect.getUpperBound(p);
+                 Type mUpper = JieReflect.getUpperBound(m);
+                 if (!isAssignable(pUpper, mUpper)) {
+                     return false;
+                 }
+                 Type pLower = JieReflect.getLowerBound(p);
+                 if (pLower == null) {
+                     return true;
+                 }
+                 Type mLower = JieReflect.getLowerBound(m);
+                 if (mLower == null) {
+                     return false;
+                 }
+                 return isAssignable(mLower, pLower);
+             }
+             if (assignee instanceof GenericArrayType) {
+                 return matchesWildcardType(p, assignee);
+             }
+             return false;
+         }
+         if (assigned instanceof GenericArrayType) {
+             GenericArrayType p = (GenericArrayType) assigned;
+             if (assignee instanceof Class<?>) {
+                 Class<?> m = (Class<?>) assignee;
+                 if (!m.isArray()) {
+                     return false;
+                 }
+                 Type pComponent = p.getGenericComponentType();
+                 Type mComponent = m.getComponentType();
+                 return matches(pComponent, mComponent);
+             }
+             if (assignee instanceof ParameterizedType) {
+                 return false;
+             }
+             if (assignee instanceof TypeVariable<?>) {
+                 return false;
+             }
+             if (assignee instanceof WildcardType) {
+                 return matchesTypeWildcard(assigned, (WildcardType) assignee);
+             }
+             if (assignee instanceof GenericArrayType) {
+                 GenericArrayType m = (GenericArrayType) assignee;
+                 Type pComponent = p.getGenericComponentType();
+                 Type mComponent = m.getGenericComponentType();
+                 return matches(pComponent, mComponent);
+             }
+             return false;
+         }
+         return false;
     }
 
-    private static boolean isAssignableFromForParameterizedType(Type t1, Type t2) {
-        if (Objects.equals(t1, t2)) {
-            return true;
-        }
-        if (t1 instanceof WildcardType) {
-            WildcardType w1 = (WildcardType) t1;
-            Type upperBound = getUpperBound(w1);
-            //? extends
-            if (upperBound != null) {
-                if (t2 instanceof WildcardType) {
-                    WildcardType w2 = (WildcardType) t2;
-                    Type upperBound2 = getUpperBound(w2);
-                    //? extends
-                    if (upperBound2 != null) {
-                        return isAssignable(upperBound, upperBound2);
-                    }
-                    return false;
-                }
-                return isAssignable(upperBound, t2);
-            }
-            Type lowerBound = getLowerBound(w1);
-            //? super
-            if (lowerBound != null) {
-                if (t2 instanceof WildcardType) {
-                    WildcardType w2 = (WildcardType) t2;
-                    Type lowerBound2 = getLowerBound(w2);
-                    //? super
-                    if (lowerBound2 != null) {
-                        return isAssignable(lowerBound2, lowerBound);
-                    }
-                    return false;
-                }
-                return isAssignable(t2, lowerBound);
+    private static boolean isAssignableTypeArgs(Type[] pArgs, Type[] mArgs) {
+        int size = Math.max(pArgs.length, mArgs.length);
+        for (int i = 0; i < size; i++) {
+            Type p = i < pArgs.length ? pArgs[i] : JieType.questionMark();
+            Type m = i < mArgs.length ? mArgs[i] : JieType.questionMark();
+            if (!isAssignable(p, m)) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
