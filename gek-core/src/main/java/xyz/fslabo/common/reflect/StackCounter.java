f48501ab89c0f749b42059e0231ff398ad6ad962
fredsuvn <fredsuvn@163.com>
2024-09-14 23:08:00 +0800

    add stack counter

---
+++ gek-core/src/main/java/xyz/fslabo/common/reflect/StackCounter.java
@@ -0,0 +1,61 @@
+package xyz.fslabo.common.reflect;
+
+/**
+ * A counter to count stack depth.
+ *
+ * @author fredsuvn
+ */
+public class StackCounter {
+
+    private int depth;
+    private int maxDepth;
+
+    /**
+     * Constructs a new counter.
+     */
+    public StackCounter() {
+        this.depth = 0;
+        this.maxDepth = 0;
+    }
+
+    /**
+     * Increments one depth, returns this.
+     *
+     * @return this
+     */
+    public StackCounter increment() {
+        depth++;
+        if (depth > maxDepth) {
+            maxDepth = depth;
+        }
+        return this;
+    }
+
+    /**
+     * Decrements one depth, returns this.
+     *
+     * @return this
+     */
+    public StackCounter decrement() {
+        depth--;
+        return this;
+    }
+
+    /**
+     * Returns current depth.
+     *
+     * @return current depth
+     */
+    public int getDepth() {
+        return depth;
+    }
+
+    /**
+     * Return the max depth that this stack counter has ever reached.
+     *
+     * @return max depth that this stack counter has ever reached
+     */
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+}
---
+++ gek-core/src/test/java/test/reflect/ReflectMiscTest.java
@@ -0,0 +1,27 @@
+package test.reflect;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import xyz.fslabo.common.reflect.StackCounter;
+
+public class ReflectMiscTest {
+
+    @Test
+    public void testStackCounter() throws Exception {
+        StackCounter counter = new StackCounter();
+        Assert.assertEquals(counter.getDepth(), 0);
+        Assert.assertEquals(counter.getMaxDepth(), 0);
+        counter.increment();
+        Assert.assertEquals(counter.getDepth(), 1);
+        Assert.assertEquals(counter.getMaxDepth(), 1);
+        counter.increment();
+        Assert.assertEquals(counter.getDepth(), 2);
+        Assert.assertEquals(counter.getMaxDepth(), 2);
+        counter.decrement();
+        Assert.assertEquals(counter.getDepth(), 1);
+        Assert.assertEquals(counter.getMaxDepth(), 2);
+        counter.increment();
+        Assert.assertEquals(counter.getDepth(), 2);
+        Assert.assertEquals(counter.getMaxDepth(), 2);
+    }
+}
